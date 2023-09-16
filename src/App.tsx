0b770ed3d5341b34c953b3eec19cd7b824a0385c
OliverMengich <kemeioliver2000@gmail.com>
2023-09-16 09:31:37 +0300

    feat: adds routes for automation, apps, devices and settings

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import Layout from './components/layout'
 import Dashboard from './pages/dashboard'
+import Devices from './pages/devices'
+import Automation from './pages/automation'
+import Apps from './pages/apps'
+import Settings from './pages/settings'
 function App() {
   const router = createBrowserRouter([
     {
@@ -10,6 +14,26 @@ function App() {
         {
           path: '/',
           element: <Dashboard/>
+        },
+        {
+          path:'/devices',
+          element: <Devices/>
+        },
+        {
+          path: '*',
+          element: <div>Not Found</div>
+        },
+        {
+          path: '/automation',
+          element: <Automation/>
+        },
+        {
+          path:'/apps',
+          element:<Apps/>
+        },
+        {
+          path:'/settings',
+          element: <Settings/>
         }
       ]
     }
