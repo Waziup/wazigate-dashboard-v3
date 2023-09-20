a6c3ddcce0917cf2601cab62afbf8bff224363c3
OliverMengich <kemeioliver2000@gmail.com>
2023-09-20 08:58:06 +0300

    feat: reusable select component and responsive mobile desing

--- src/pages/automation.tsx
+++ src/pages/automation.tsx
@@ -1,18 +1,25 @@
-import { Add,Mode, MoreVert } from "@mui/icons-material";
-import { Box, Button,FormControl,NativeSelect,Typography } from "@mui/material";
+import { Add,Check,Clear,Mode, MoreVert, } from "@mui/icons-material";
+import { Box, Button,FormControl,NativeSelect,Stack,Typography } from "@mui/material";
 import { RowContainerBetween } from "./dashboard";
 import { DEFAULT_COLORS } from "../constants";
 import React, { ChangeEvent, } from "react";
-
-const SelectElement = ({handleChange,title,conditions, value}:{title:string,handleChange:(event: ChangeEvent<HTMLSelectElement>)=>void,conditions:string[] | number[], value: string})=>(
+import { useOutletContext } from "react-router-dom";
+import { IconStyle } from "../components/layout/sidebar";
+interface HTMLSelectProps extends React.SelectHTMLAttributes<HTMLSelectElement> {
+    handleChange:(event: ChangeEvent<HTMLSelectElement>)=>void,
+    title:string,
+    conditions:string[] | number[], 
+    value: string
+}
+const SelectElement = ({handleChange,title,conditions, value}:HTMLSelectProps)=>(
     <Box minWidth={120} mx={2}>
         <Typography fontSize={12} color={DEFAULT_COLORS.secondary_black}>{title}</Typography>
         <FormControl fullWidth>
             <NativeSelect
                 defaultValue={30}
                 inputProps={{
-                name: 'age',
-                id: 'uncontrolled-native',
+                    name: 'age',
+                    id: 'uncontrolled-native',
                 }}
                 value={value}
                 onChange={handleChange}
@@ -29,39 +36,78 @@ function Automation() {
     const handleChange = (event: ChangeEvent<HTMLSelectElement>) => {
         setAge(event.target.value);
     };
+    const [matches] = useOutletContext<[matches: boolean]>()
     return (
-        <Box p={3} sx={{ height:'100%'}}>
-            <RowContainerBetween>
-                <Box>
-                    <Typography fontWeight={700} color={'black'}>Automation</Typography>
-                    <Typography sx={{color:DEFAULT_COLORS.secondary_black}}>Setup your Actuation Logics</Typography>
-                </Box>
-                <Button variant={'contained'}>
-                    <Add />
-                    New Rule
-                </Button>
-            </RowContainerBetween>
-            <Box py={2}>
-                <Box sx={{ position:'relative', bgcolor: 'white', borderRadius:2, }}>
-                    <Box sx={{position:'absolute',top:-5,my:-1,}} borderRadius={1} mx={1} bgcolor={DEFAULT_COLORS.primary_blue}>
-                        <Typography  mx={1} color={'white'} component={'span'}>Rule 1</Typography>
-                    </Box>
-                    <Box mx={2} py={2} display={'flex'} justifyContent={'space-evenly'} alignItems={'center'} >
-                        <Typography color={DEFAULT_COLORS.primary_black} p={1} height={'10%'} fontSize={14} borderRadius={'100%'} bgcolor={'#D9D9D9'}>if</Typography>
-                        <SelectElement title={'Sensor'} handleChange={handleChange} conditions={['Tempeature','Level','Humidity']} value={age} />
-                        <SelectElement title={'Condition'} handleChange={()=>{}} conditions={['>','<','==']} value={''} />
-                        <SelectElement title={'Values'} handleChange={()=>{}} conditions={[32,33,34,35,26]} value={''} />
-                        <Typography color={DEFAULT_COLORS.primary_black} p={1} height={'10%'} fontSize={14} borderRadius={'100%'} bgcolor={'#D9D9D9'}>do</Typography>
-                        <SelectElement title={'Actuator'} handleChange={()=>{}} conditions={['WaterPump','<','==']} value={''} />
-                        <SelectElement title={'Action'} handleChange={()=>{}} conditions={['on','off']} value={''} />
-                        <Box>
-                            <Mode sx={{color:'black',mx:1}}/>
-                            <MoreVert sx={{color:'black',mx:1}}/>
+        <>
+            {
+                matches?(
+                    <Box p={3} sx={{ height:'100%'}}>
+                        <RowContainerBetween>
+                            <Box>
+                                <Typography fontWeight={700} color={'black'}>Automation</Typography>
+                                <Typography sx={{color:DEFAULT_COLORS.secondary_black}}>Setup your Actuation Logics</Typography>
+                            </Box>
+                            <Button variant={'contained'}>
+                                <Add />
+                                New Rule
+                            </Button>
+                        </RowContainerBetween>
+                        <Box py={2}>
+                            <Box sx={{ position:'relative', bgcolor: 'white', borderRadius:2, }}>
+                                <Box sx={{position:'absolute',top:-5,my:-1,}} borderRadius={1} mx={1} bgcolor={DEFAULT_COLORS.primary_blue}>
+                                    <Typography  mx={1} color={'white'} component={'span'}>Rule 1</Typography>
+                                </Box>
+                                <Box mx={2} py={2} display={'flex'} justifyContent={'space-evenly'} alignItems={'center'} >
+                                    <Box component={'img'} src={'/if.svg'} height={25} width={'10%'} />
+                                    <SelectElement title={'Sensor'} handleChange={handleChange} conditions={['Tempeature','Level','Humidity']} value={age} />
+                                    <SelectElement title={'Condition'} handleChange={()=>{}} conditions={['>','<','==']} value={''} />
+                                    <SelectElement title={'Values'} handleChange={()=>{}} conditions={[32,33,34,35,26]} value={''} />
+                                    <Box component={'img'} src={'/do.svg'} height={25} width={'10%'} />
+                                    <SelectElement title={'Actuator'} handleChange={()=>{}} conditions={['WaterPump','<','==']} value={''} />
+                                    <SelectElement title={'Action'} handleChange={()=>{}} conditions={['on','off']} value={''} />
+                                    <Box>
+                                        <Mode sx={{color:'black',mx:1}}/>
+                                        <MoreVert sx={{color:'black',mx:1}}/>
+                                    </Box>
+                                </Box>     
+                            </Box>
                         </Box>
-                    </Box>     
-                </Box>
-            </Box>
-        </Box>
+                    </Box>
+                ):(
+                    <Box position={'relative'} height={'100%'} width={'100%'}>
+                        <Stack flexDirection={'column'} width={'100%'} mt={4} display={'flex'} alignItems={'center'}>
+                            <Box onClick={()=>{}} sx={{cursor:'pointer',":hover":{bgcolor:'#f5f5f5'},width:'90%',py:2, height: '100%',position:'relative', bgcolor: 'white', borderRadius:2, }}>
+                                <Box sx={{position:'absolute',top:-5,my:-1,}} borderRadius={1} mx={1} bgcolor={DEFAULT_COLORS.primary_blue}>
+                                    <Typography fontSize={16} mx={1} color={'white'} component={'span'}>Rules 1</Typography>
+                                </Box>
+                                <Box sx={{px:2,}}>
+                                    <RowContainerBetween>
+                                        <Box component={'img'} sx={{m:1}} src={'/if.svg'} height={'10%'} width={'10%'} />
+                                        <Box>
+                                            <Check sx={{...IconStyle,mx:1}} />
+                                            <Clear sx={{...IconStyle,mx:1}} />
+                                        </Box>
+                                    </RowContainerBetween>
+                                </Box>
+                                <SelectElement title={'Sensor'} handleChange={handleChange} conditions={['Tempeature','Level','Humidity']} value={age} />
+                                <SelectElement title={'Condition'} handleChange={()=>{}} conditions={['>','<','==']} value={''} />
+                                <SelectElement title={'Values'} handleChange={()=>{}} conditions={[32,33,34,35,26]} value={''} />
+                                <Box sx={{ px:2,}}>
+                                    <Box component={'img'} sx={{m:1}} src={'/do.svg'} height={'10%'} width={'10%'} />
+                                </Box>
+                                <Box my={2}>
+                                    <SelectElement title={'Actuator'} handleChange={()=>{}} conditions={['WaterPump','<','==']} value={''} />
+                                    <SelectElement title={'Action'} handleChange={()=>{}} conditions={['on','off']} value={''} />
+                                </Box>
+                            </Box>
+                        </Stack>
+                        <Button variant={'contained'} sx={{position:'absolute',bottom:50,right:10,}}>
+                            <Add/>
+                        </Button>
+                    </Box>
+                )
+            }
+        </>
     );
 }
 
