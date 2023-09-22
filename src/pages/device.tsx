c1eac0cbc11a17d8b1bc7794cca5b4f2a516ea66
OliverMengich <kemeioliver2000@gmail.com>
2023-09-22 19:44:06 +0300

    feat: adds graph display to graph

--- src/pages/device.tsx
+++ src/pages/device.tsx
@@ -3,7 +3,7 @@ import { Box, Typography, Button, Chip, emphasize, styled, Breadcrumbs } from "@
 import { RowContainerBetween } from "./dashboard";
 import EnhancedTable from "../components/devicetable";
 import { useNavigate } from "react-router-dom";
-const StyledBreadcrumb = styled(Chip)(({ theme }) => {
+export const StyledBreadcrumb = styled(Chip)(({ theme }) => {
     const backgroundColor =
       theme.palette.mode === 'light'
         ? theme.palette.grey[100]
@@ -22,7 +22,7 @@ const StyledBreadcrumb = styled(Chip)(({ theme }) => {
       },
     };
   }) as typeof Chip; // TypeScript only: need a type cast here because https://github.com/Microsoft/TypeScript/issues/26591
-  
+import Chart from 'react-apexcharts';  
 function Device() {
     function handleClick(event: React.MouseEvent<Element, MouseEvent>) {
         event.preventDefault();
@@ -31,8 +31,8 @@ function Device() {
     const navigate = useNavigate();
     const handleNav = (path:string)=>{navigate(path)}
     return (
-        <Box p={3} sx={{ height:'100%'}}>
-            <RowContainerBetween>
+        <Box sx={{ height:'100%'}}>
+            <RowContainerBetween >
                 <Box>
                     <Typography fontWeight={700} color={'black'}>Device 1</Typography>
                     <div role="presentation" onClick={handleClick}>
@@ -57,7 +57,40 @@ function Device() {
                     SETTINGS
                 </Button>
             </RowContainerBetween>
-            <EnhancedTable  />
+            <Box bgcolor={'#fff'} height={'100%'} display={'flex'} width={'100%'} mt={9} flexDirection={'column'} alignItems={'center'}  justifyContent={'center'}>
+                <Box width={'80%'} mb={3}>
+                    <RowContainerBetween additionStyles={{width:'100%'}}>
+                        <Typography>Current Temperature</Typography>
+                        <Typography>25.5 °C</Typography>
+                    </RowContainerBetween>
+                    <Chart
+                        options={{
+                            chart: {
+                                id: "basic-bar"
+                            },
+                            xaxis: {
+                                categories: [1,2,3,4,5,6,7,8,9,10]
+                            },
+                            stroke:{
+                                curve:'smooth',
+                                width:2
+                            }
+                        }}
+                        series={[
+                            {
+                                name: "series-1",
+                                data: [30,40,45,50,49,60,70,91,125,100]
+                            }
+                        ]}
+                        type="line"
+                        width={'100%'}
+                        height={300}
+                    />
+                </Box>
+                <Box bgcolor={'#fff'} width={'80%'}>
+                    <EnhancedTable  />
+                </Box>
+            </Box>
         </Box>
     );
 }
