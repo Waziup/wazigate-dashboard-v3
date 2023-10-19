0c7c97b48865d88038b70b1b8aee118fd88ed444
Oliver Kipkemei <55626791+OliverMengich@users.noreply.github.com>
2023-10-19 19:35:37 +0300

    feat: sets common grid item

--- src/pages/Apps.tsx
+++ src/pages/Apps.tsx
@@ -39,6 +39,14 @@ const DropDown = ({handleChange,matches, age}:{matches:boolean, handleChange:()=
         </Select>
     </FormControl>
 );
+const GridItem=({children}:{children:React.ReactNode})=>(
+    <Grid item md={6} lg={4} xl={4} sm={6} xs={12} minHeight={100} my={1} px={1} >
+        <Box minHeight={100} sx={{px:2, py:1, position:'relative', bgcolor: 'white', borderRadius:2, }}>
+            {children}
+            <Button sx={{fontWeight:'700'}}>OPEN</Button>
+        </Box>
+    </Grid>
+)
 function Apps() {
     const [matches] = useOutletContext<[matches: boolean]>();
     const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
@@ -60,79 +68,75 @@ function Apps() {
                 <DropDown matches={matches} handleChange={()=>{}} age={''} />
             </RowContainerBetween>
             <Grid container spacing={2} py={2}>
-                <Grid item minHeight={100} my={1} px={1} xs={matches?4:12}>
-                    <Box minHeight={100} sx={{px:2, py:1, position:'relative', bgcolor: 'white', borderRadius:2, }}>
-                        <Box px={.4} display={'flex'} alignItems={'center'} sx={{position:'absolute',top:-5,my:-1,}} borderRadius={1} mx={1} bgcolor={DEFAULT_COLORS.primary_blue}>
-                            <Box component={'img'} src='/wazi_sig.svg' />
-                            <Typography  mx={1} color={'white'} component={'span'}>Waziup App</Typography>
+                <GridItem>
+                    <Box px={.4} display={'flex'} alignItems={'center'} sx={{position:'absolute',top:-5,my:-1,}} borderRadius={1} mx={1} bgcolor={DEFAULT_COLORS.primary_blue}>
+                        <Box component={'img'} src='/wazi_sig.svg' />
+                        <Typography  mx={1} color={'white'} component={'span'}>Waziup App</Typography>
+                    </Box>
+                    <Box display={'flex'} py={2}  justifyContent={'space-between'}>
+                        <Box>
+                            <NormalText title="Waziup App" />
+                            <Typography color={DEFAULT_COLORS.secondary_black} fontWeight={300}>wazigate-edge</Typography>
                         </Box>
-                        <Box display={'flex'} py={2}  justifyContent={'space-between'}>
-                            <Box>
-                                <NormalText title="Waziup App" />
-                                <Typography color={DEFAULT_COLORS.secondary_black} fontWeight={300}>wazigate-edge</Typography>
-                            </Box>
-                            <Box>
-                                <Button id="demo-positioned-button"
-                                    aria-controls={open ? 'demo-positioned-menu' : undefined}
-                                    aria-haspopup="true"
-                                    aria-expanded={open ? 'true' : undefined}
-                                    onClick={handleClick}
-                                    >
-                                    <MoreVert sx={{color:'black'}}/>
-                                </Button>
-                                <Menu
-                                    id="demo-positioned-menu"
-                                    aria-labelledby="demo-positioned-button"
-                                    anchorEl={anchorEl}
-                                    open={open}
-                                    onClose={handleClose}
-                                    anchorOrigin={{
-                                    vertical: 'top',
-                                    horizontal: 'left',
-                                    }}
-                                    transformOrigin={{
-                                    vertical: 'top',
-                                    horizontal: 'left',
-                                    }}
+                        <Box>
+                            <Button id="demo-positioned-button"
+                                aria-controls={open ? 'demo-positioned-menu' : undefined}
+                                aria-haspopup="true"
+                                aria-expanded={open ? 'true' : undefined}
+                                onClick={handleClick}
                                 >
-                                    <MenuItem onClick={handleClose}>
-                                        <ListItemIcon>
-                                            <SettingsTwoTone fontSize="small" />
-                                        </ListItemIcon>
-                                        <ListItemText>Settings</ListItemText>
-                                    </MenuItem>
-                                    <MenuItem onClick={handleClose}>
-                                        <ListItemIcon>
-                                            <DeleteForever fontSize="small" />
-                                        </ListItemIcon>
-                                        <ListItemText>Uninstall</ListItemText>
-                                    </MenuItem>
-                                </Menu>
-                            </Box>
+                                <MoreVert sx={{color:'black'}}/>
+                            </Button>
+                            <Menu
+                                id="demo-positioned-menu"
+                                aria-labelledby="demo-positioned-button"
+                                anchorEl={anchorEl}
+                                open={open}
+                                onClose={handleClose}
+                                anchorOrigin={{
+                                vertical: 'top',
+                                horizontal: 'left',
+                                }}
+                                transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'left',
+                                }}
+                            >
+                                <MenuItem onClick={handleClose}>
+                                    <ListItemIcon>
+                                        <SettingsTwoTone fontSize="small" />
+                                    </ListItemIcon>
+                                    <ListItemText>Settings</ListItemText>
+                                </MenuItem>
+                                <MenuItem onClick={handleClose}>
+                                    <ListItemIcon>
+                                        <DeleteForever fontSize="small" />
+                                    </ListItemIcon>
+                                    <ListItemText>Uninstall</ListItemText>
+                                </MenuItem>
+                            </Menu>
                         </Box>
-                        <Typography color={DEFAULT_COLORS.secondary_black}>Status: <span color='red'>Running</span></Typography>
-                        <Typography color={DEFAULT_COLORS.secondary_black}>Waziup firmware for Edge Computing</Typography>
-                        <Button sx={{fontWeight:'700'}}>OPEN</Button>
                     </Box>
-                </Grid>
-                <Grid item minHeight={100} my={1} px={1} xs={matches?4:12}>
-                    <Box minHeight={100} sx={{px:2, py:1, position:'relative', bgcolor: 'white', borderRadius:2, }}>
-                        <Box px={.4} display={'flex'} alignItems={'center'} sx={{position:'absolute',top:-5,my:-1,}} borderRadius={1} mx={1} bgcolor={DEFAULT_COLORS.orange}>
-                            <Terminal />
-                            <Typography  mx={1} color={'white'} component={'span'}>Custom App</Typography>
-                        </Box>
-                        <Box display={'flex'} py={2}  justifyContent={'space-between'}>
-                            <Box>
-                                <NormalText title="Waziup App" />
-                                <Typography color={DEFAULT_COLORS.secondary_black} fontWeight={300}>wazigate-edge</Typography>
-                            </Box>
-                            <MoreVert sx={{color:'black'}}/>
-                        </Box>
-                        <Typography color={DEFAULT_COLORS.secondary_black}>Status: <span color='red'>Running</span></Typography>
-                        <Typography color={DEFAULT_COLORS.secondary_black}>Waziup firmware for Edge Computing</Typography>
-                        <Button sx={{fontWeight:'700'}}>OPEN</Button>
+                    <Typography color={DEFAULT_COLORS.secondary_black}>Status: <span color='red'>Running</span></Typography>
+                    <Typography color={DEFAULT_COLORS.secondary_black}>Waziup firmware for Edge Computing</Typography>
+                   
+                </GridItem>
+                <GridItem>
+                    <Box px={.4} display={'flex'} alignItems={'center'} sx={{position:'absolute',top:-5,my:-1,}} borderRadius={1} mx={1} bgcolor={DEFAULT_COLORS.orange}>
+                        <Terminal />
+                        <Typography  mx={1} color={'white'} component={'span'}>Custom App</Typography>
                     </Box>
-                </Grid>
+                    <Box display={'flex'} py={2}  justifyContent={'space-between'}>
+                        <Box>
+                            <NormalText title="Waziup App" />
+                            <Typography color={DEFAULT_COLORS.secondary_black} fontWeight={300}>wazigate-edge</Typography>
+                        </Box>
+                        <MoreVert sx={{color:'black'}}/>
+                    </Box>
+                    <Typography color={DEFAULT_COLORS.secondary_black}>Status: <span color='red'>Running</span></Typography>
+                    <Typography color={DEFAULT_COLORS.secondary_black}>Waziup firmware for Edge Computing</Typography>
+                    
+                </GridItem>
             </Grid>
         </Box>
     );
