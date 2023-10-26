3786f889f156e20bf2171130b1139a3acd8adaed
Oliver Kipkemei <55626791+OliverMengich@users.noreply.github.com>
2023-10-26 10:49:02 +0300

    fix: fontsize responsive

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -20,31 +20,35 @@ const DeviceStatus = ()=>(
         <BasicTable/>
     </Box>
 );
+const TextItem = ({text}:{text:string})=>(
+    <Typography sx={{fontSize:[10,10,12,13,10],color:DEFAULT_COLORS.secondary_black,fontWeight:300}} >{text}</Typography>
+
+)
 const AppStatus = ()=>(
     <Box sx={{ height: '100%', bgcolor: 'white', borderRadius:2, p: 2}}>
         <NormalText title="App Status" />
-        <Stack>
+        <Stack width={'100%'} height={'100%'}>
             <RowContainerBetween>
                 <RowContainerNormal >
-                    <WaterDrop sx={{ fontSize: 40,color:DEFAULT_COLORS.primary_blue }} />
+                    <WaterDrop sx={{ fontSize: [20,35,38,40,40],color:'info.main' }} />
                     <Box>
                         <Typography color={'black'} fontWeight={300}>Intelliris</Typography>
-                        <Typography color={DEFAULT_COLORS.secondary_black} fontWeight={300}>Last active 3h ago</Typography>
+                        <TextItem text="Last active 3h ago"/>
                     </Box>
                 </RowContainerNormal>
-                <Typography color={DEFAULT_COLORS.primary_blue} fontWeight={300}>active</Typography> 
+                <Typography sx={{color:'info.main',fontWeight:300, fontSize:[10,12,16,15,10]}}>active</Typography> 
             </RowContainerBetween>
             <RowContainerBetween>
                 <RowContainerNormal>
-                    <Box sx={{ bgcolor:DEFAULT_COLORS.orange,  borderRadius:'50%',height:35,width:35, }}>
-                        <WifiTethering sx={{ textAlign:'center',  color:'#fff', }} />
+                    <Box sx={{mr:.5, bgcolor:'secondary.main', textAlign:'center', borderRadius:'50%',height:[20,35,38,40,40],width:[20,35,38,40,40], }}>
+                        <WifiTethering sx={{ m:'auto',mt:.5,  color:'#fff', }} />
                     </Box>
                     <Box>
                         <Typography color={'black'} fontWeight={300}>LoraWAN</Typography>
-                        <Typography color={DEFAULT_COLORS.secondary_black} fontWeight={300}>by Waziup</Typography>
+                        <TextItem text="by Waziup"/>
                     </Box>
                 </RowContainerNormal>
-                <Typography color={'#CCC400'} fontWeight={300}>offline</Typography>    
+                <Typography color={'#CCC400'} fontSize={[10,12,16,15,10]} fontWeight={300}>offline</Typography>    
             </RowContainerBetween>
         </Stack>
     </Box>
@@ -57,7 +61,7 @@ function Dashboard() {
         <>
             {
                 matches?(
-                    <Box p={3} sx={{ height:'100%'}}>
+                    <Box p={3} sx={{width:'100%', height:'100%'}}>
                         <Typography color={'black'} fontWeight={700}>Gateway Dashboard</Typography>
                         <Stack direction={'row'} mt={2} spacing={2}>
                             <Item color={DEFAULT_COLORS.primary_blue} title="Gateway Status" more="Good" >
@@ -71,10 +75,10 @@ function Dashboard() {
                             </Item>
                         </Stack>
                         <Grid mt={2} container spacing={2}>
-                            <Grid item py={6} xs={8}>
+                            <Grid item py={6} sm={7} md={8} >
                                 <DeviceStatus />
                             </Grid>
-                            <Grid py={6} item xs={4}>
+                            <Grid py={6} item sm={5} md={4} >
                                 <AppStatus />
                             </Grid>
                         </Grid>
