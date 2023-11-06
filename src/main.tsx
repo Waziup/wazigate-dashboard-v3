7424411852bd027ccf311dea66b943250f9f1067
Oliver Kipkemei <55626791+OliverMengich@users.noreply.github.com>
2023-11-06 07:12:18 +0300

    chore(api): adds waziup connect

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css';
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
+import * as waziup from 'waziup';
+import { BACKEND_URL } from './constants/index.ts';
 
-  </React.StrictMode>,
-)
+declare global {
+    interface Window {
+        wazigate: waziup.Waziup;
+    }
+}
+
+waziup.connect({
+    host: BACKEND_URL,
+}).then((wazigate: waziup.Waziup) => {
+    window.wazigate = wazigate;
+
+    ReactDOM.createRoot(document.getElementById('root')!).render(
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>,
+    )
+})
