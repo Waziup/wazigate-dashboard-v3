71453f2e8483f1268667333a09df58ae9070a04e
Oliver Kipkemei <55626791+OliverMengich@users.noreply.github.com>
2023-11-06 12:35:37 +0300

    feat: not connected status

--- src/main.tsx
+++ src/main.tsx
@@ -10,15 +10,18 @@ declare global {
         wazigate: waziup.Waziup;
     }
 }
-
+console.log(waziup);
 waziup.connect({
     host: BACKEND_URL,
 }).then((wazigate: waziup.Waziup) => {
     window.wazigate = wazigate;
-
+    console.log('Connected to Wazigate')
     ReactDOM.createRoot(document.getElementById('root')!).render(
         <React.StrictMode>
             <App />
         </React.StrictMode>,
     )
+}).catch(()=>{
+    console.log('Cannot connect to Wazigate');
+    (document.getElementById("dashboard") as HTMLElement).innerHTML = "<div style='margin-top: 20%;color:black; text-align: center;border: 1px solid #BBB;border-radius: 5px;padding: 5%;margin-left: 10%;margin-right: 10%;background-color: #EEE;'><h1>Wazigate is not accessible...</h1></div>";
 })
