ed0fcf314cd602d3d8e7c0d22df17cd2bd90b6ac
Oliver Kipkemei <55626791+OliverMengich@users.noreply.github.com>
2023-11-21 09:53:22 +0300

    feat: dynamic elements and elements

--- src/pages/Devices.tsx
+++ src/pages/Devices.tsx
@@ -1,6 +1,6 @@
-import { Box, Button,Grid,CardContent,Typography, } from '@mui/material';
+import { Box, Button,Grid,CardContent,Typography, Icon, } from '@mui/material';
 import RowContainerBetween from '../components/RowContainerBetween';
-import { Add,CompareArrows,DeviceThermostat, OnlinePrediction, Sensors } from '@mui/icons-material';
+import { Add, Sensors } from '@mui/icons-material';
 import { DEFAULT_COLORS } from '../constants';
 import { useNavigate,  } from 'react-router-dom';
 import { useEffect,useState } from 'react';
@@ -9,6 +9,17 @@ const IconStyle = {
     cursor:'pointer',
     color:'black',
 }
+//CompareArrows, Sensors, DeviceThermostat,OnlinePrediction
+
+export const SensorInfo = ({text,name,onClick,iconname}:{text:string,name:string,onClick:()=>void, iconname:string})=>(
+    <RowContainerBetween onClick={onClick} additionStyles={{my:2,py:1,px:.5, ":hover":{bgcolor:'#f5f5f5'}}}>                                            
+        <Box sx={{display:'flex',width:'50%'}}>
+            <Icon sx={{ fontSize: 18,color:DEFAULT_COLORS.primary_black }} >{iconname}</Icon>
+            <Typography color={'black'} ml={1} fontSize={12} fontWeight={300}>{name}</Typography>
+        </Box>
+        <Typography color={DEFAULT_COLORS.primary_blue}fontSize={14} fontWeight={300}>{text} </Typography>
+    </RowContainerBetween>
+)
 function Devices() {
     const navigate = useNavigate();
     const [devices, setDevices] = useState<Device[]>([]);
@@ -26,15 +37,15 @@ function Devices() {
             </RowContainerBetween>
             <Grid container my={2} spacing={2}>
                 {
-                    devices.map((device)=>(
-                        <Grid item md={6} lg={4} xl={4} sm={8} xs={12} >
-                            <Box onClick={()=>{navigate(`/devices/${device.id}`,{state:{...device,title:device.name}})}} sx={{cursor:'pointer',":hover":{bgcolor:'#fffff1'}, height: '100%',position:'relative', bgcolor: 'white', borderRadius:2, }}>
+                    devices.map((device,id)=>(
+                        <Grid key={id} item md={6} lg={4} xl={4} sm={8} xs={12} >
+                            <Box  sx={{cursor:'pointer',":hover":{bgcolor:'#fffff1'}, height: '100%',position:'relative', bgcolor: 'white', borderRadius:2, }}>
                                 <Box sx={{display:'flex',alignItems:'center', position:'absolute',top:-5,my:-1,borderRadius:1,mx:1,bgcolor:DEFAULT_COLORS.primary_blue}}>
                                     <Sensors sx={{fontSize:15, color:'#fff'}}/>
                                     <Typography fontSize={13} mx={1} color={'white'} component={'span'}>WaziDev</Typography>
                                 </Box>
-                                <Box sx={{borderBottom:'1px solid black',py:1.5, px:2,}}>
-                                    <RowContainerBetween>
+                                <Box onClick={()=>{navigate(`/devices/${device.id}`,{state:{...device,title:device.name}})}} sx={{borderBottom:'1px solid black',py:1.5,":hover":{py:1.5}, px:2,}}>
+                                    <RowContainerBetween additionStyles={{":hover":{bgcolor:'#f5f5f5',px:1},}} >
                                         <Box>
                                             <Typography color={'black'} fontWeight={700}>{device.name.slice(0,10)+'....'}</Typography>
                                             <Typography color={DEFAULT_COLORS.secondary_black} fontSize={15} fontWeight={300}>Last updated: 10 seconds</Typography>
@@ -47,39 +58,20 @@ function Devices() {
                                         New Interface
                                     </Button>
                                     {
-                                        device.sensors.length>0?device.sensors.map(()=>(
-                                            <Box >
-                                                <RowContainerBetween additionStyles={{my:2}}>
-                                                    <Box sx={{display:'flex',width:'50%'}}>
-                                                        <DeviceThermostat sx={{ fontSize: 20,color:DEFAULT_COLORS.primary_black }} />
-                                                        <Typography color={'black'} mx={1} fontSize={12} fontWeight={300}>Room_1 Temp</Typography>
-                                                    </Box>
-                                                    <Typography color={DEFAULT_COLORS.primary_blue}fontSize={14} fontWeight={300}>25&deg;C </Typography>
-                                                </RowContainerBetween>
-                                                <RowContainerBetween additionStyles={{my:2}}>
-                                                    
-                                                    <Box sx={{display:'flex',width:'50%'}}>
-                                                        <OnlinePrediction sx={{ fontSize: 20,color:DEFAULT_COLORS.primary_black }} />
-                                                        <Typography color={'black'} mx={1} fontSize={12} fontWeight={300}>Object Detector</Typography>
-                                                    </Box>
-                                                    <Typography color={DEFAULT_COLORS.primary_blue}fontSize={14} fontWeight={300}>On </Typography>
-                                                </RowContainerBetween>
-                                                <RowContainerBetween additionStyles={{my:2}}>
-                                                    
-                                                    <Box sx={{display:'flex',width:'50%'}}>
-                                                        <DeviceThermostat sx={{ fontSize: 20,color:DEFAULT_COLORS.primary_black }} />
-                                                        <Typography color={'black'} mx={1} fontSize={12} fontWeight={300}>Thermometer</Typography>
-                                                    </Box>
-                                                    <Typography color={DEFAULT_COLORS.primary_blue}fontSize={14} fontWeight={300}>36&deg;C </Typography>
-                                                </RowContainerBetween>
-                                                <RowContainerBetween additionStyles={{my:2}}>
-                                                    
-                                                    <Box sx={{display:'flex',width:'50%'}}>
-                                                        <CompareArrows sx={{ fontSize: 20,color:DEFAULT_COLORS.primary_black }} />
-                                                        <Typography color={'black'} ml={1} fontSize={12} fontWeight={300}>Soil Pressure Loan</Typography>
-                                                    </Box>
-                                                    <Typography color={DEFAULT_COLORS.primary_blue}fontSize={14} fontWeight={300}>On </Typography>
-                                                </RowContainerBetween>
+                                        device.sensors.length>0?device.sensors.map((sensor)=>(
+                                            <Box key={sensor.id}>
+                                                <SensorInfo onClick={()=>{console.log('Navigation handler'); navigate(`/devices/${device.id}/sensors/${sensor.id}`,{state:{devicename:device.name,sensorId:sensor.id, deviceId:device.id,sensorname:sensor.name}})}} iconname='device_thermostat' name={sensor.name} text='25&deg;C' />
+                                            </Box>
+                                        )):(
+                                            <Box my={2}>
+                                                <Typography sx={{textAlign:'center',fontWeight:'bold'}}>This device has no sensor</Typography>
+                                            </Box>
+                                        )
+                                    }
+                                    {
+                                        device.actuators.length>0?device.actuators.map((act)=>(
+                                            <Box key={act.id}>
+                                                <SensorInfo onClick={()=>{console.log('Navigation handler'); navigate(`/devices/${device.id}/sensors/${act.id}`,{state:{deviceId:device.id,actuatorname:act.name}})}} iconname='precision_manufacturing' name={act.name} text='25&deg;C' />
                                             </Box>
                                         )):(
                                             <Box my={2}>
