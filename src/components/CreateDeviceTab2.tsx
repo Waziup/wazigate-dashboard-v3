d5e9f2032e7c19487643c4bb659a9df88d98bd75
Oliver Kipkemei <55626791+OliverMengich@users.noreply.github.com>
2023-11-30 12:21:57 +0300

    feat: includes option to make it lorawan device

--- src/components/CreateDeviceTab2.tsx
+++ src/components/CreateDeviceTab2.tsx
@@ -1,23 +1,36 @@
 import { Box, Typography } from "@mui/material";
 import { SelectElementString } from "../pages/Automation";
-import { AddTextShow } from "../pages/DeviceSettings";
 import RowContainerBetween from "./RowContainerBetween";
 import { Android12Switch } from "./Switch";
-
-export default function CreateDeviceTabTwo({selectedValue}:{selectedValue:string}){
+import { AddCircleOutline } from "@mui/icons-material";
+const AddTextShow=({text,placeholder,}:{text:string,placeholder:string})=>(
+    <Box sx={{my:2}}>
+        <Box sx={{display:'flex',justifyContent:'space-between',alignItems:'center', borderBottom:'1px solid #ccc'}}>
+            <input placeholder={text} style={{border:'none',color:'#757474',fontWeight:200, outline:'none',width:'100%',padding:'6px 0'}} />
+            <AddCircleOutline sx={{color:'#292F3F', fontSize:20}} />
+        </Box>
+        <Typography fontSize={10} my={.5} color={'#292F3F'} fontWeight={200}>{placeholder}</Typography>
+    </Box>
+)
+export default function CreateDeviceTabTwo({selectedValue,changeMakeLoraWAN, handleChange,makeLoraWAN}:{selectedValue:string, handleChange:()=>void,changeMakeLoraWAN:()=>void, makeLoraWAN:boolean}){
     return(
         <Box>
-            <SelectElementString mx={0} title='Device Codec' value={selectedValue} handleChange={()=>{}} conditions={['JSON','b']} />
+            <SelectElementString mx={0} title='Device Codec' value={selectedValue} handleChange={handleChange} conditions={['JSON','b']} />
             <RowContainerBetween additionStyles={{my:1}}>
                 <Typography fontSize={13}>LoRAWAN Device</Typography>
-                <Android12Switch color='info' />
+                <Android12Switch onChange={changeMakeLoraWAN} color='info' />
             </RowContainerBetween>
-            <Box my={2}>
-                <SelectElementString mx={0} title={'Label'} handleChange={()=>{}} conditions={['Tempeature','Level','Humidity']} value={'Temperature'} />
-                <AddTextShow text={'Device Addr (Device Address)'}  placeholder={'8 digits required, got 0'} />
-                <AddTextShow text={'NwkSKey(Network Session Key)'}  placeholder={'32 digits required, got 0'} />
-                <AddTextShow text={'AppKey (App Key)'}  placeholder={'32 digits required, got 0'} />
-            </Box>
+            {
+                makeLoraWAN && (
+                    <Box my={2}>
+                        <SelectElementString mx={0} title={'Label'} handleChange={()=>{}} conditions={['Tempeature','Level','Humidity']} value={'Temperature'} />
+                        <AddTextShow text={'Device Addr (Device Address)'}  placeholder={'8 digits required, got 0'} />
+                        <AddTextShow text={'NwkSKey(Network Session Key)'}  placeholder={'32 digits required, got 0'} />
+                        <AddTextShow text={'AppKey (App Key)'}  placeholder={'32 digits required, got 0'} />
+                    </Box>
+                )
+            }
+            
         </Box>
     )
     
