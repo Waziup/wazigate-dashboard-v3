1443b327eb3d029c2274de4c0dbad7d11d75bfc0
Oliver Kipkemei <55626791+OliverMengich@users.noreply.github.com>
2024-01-08 10:35:40 +0300

    fix: passes option to generate lorawan options

--- src/components/AddTextInput.tsx
+++ src/components/AddTextInput.tsx
@@ -1,5 +1,5 @@
 import { AddCircleOutline } from "@mui/icons-material";
-import { Box, Typography } from "@mui/material";
+import { Box, Tooltip, Typography } from "@mui/material";
 interface AddTextProps{
     text:string
     placeholder:string
@@ -7,13 +7,22 @@ interface AddTextProps{
     onTextInputChange?:(e:React.ChangeEvent<HTMLInputElement>)=>void
     name?:string
 }
-
-export default function AddTextShow({text,name, placeholder,onTextInputChange,textInputValue}:AddTextProps){
+interface AddTextProps{
+    text:string
+    placeholder:string
+    textInputValue?:string
+    onTextInputChange?:(e:React.ChangeEvent<HTMLInputElement>)=>void
+    name?:string
+    autoGenerateHandler:(title:"devAddr"|"nwkSEncKey"|"appSKey")=>void
+}
+export default function AddTextShow({text,name,autoGenerateHandler, placeholder,onTextInputChange,textInputValue}:AddTextProps){
     return (
         <Box sx={{my:2}}>
             <Box sx={{display:'flex',justifyContent:'space-between',alignItems:'center', borderBottom:'1px solid #ccc'}}>
                 <input required name={name} value={textInputValue} onChange={onTextInputChange} placeholder={text} style={{border:'none',color:'#757474',fontWeight:200, outline:'none',width:'100%',padding:'6px 0'}} />
-                <AddCircleOutline sx={{color:'#292F3F', fontSize:20}} />
+                <Tooltip onClick={()=>autoGenerateHandler(name as "devAddr"|"nwkSEncKey"|"appSKey")} title={"AutoGenerate "+name}>
+                    <AddCircleOutline sx={{color:'#292F3F', fontSize:20}} />
+                </Tooltip>
             </Box>
             <Typography fontSize={10} my={.5} color={'#292F3F'} fontWeight={200}>{placeholder}</Typography>
         </Box>
