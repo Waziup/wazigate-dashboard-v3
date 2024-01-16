66f74a01ef3d56f13e5d335861add19033dd0228
Oliver Kipkemei <55626791+OliverMengich@users.noreply.github.com>
2024-01-16 16:21:12 +0300

    feat: ontologies added to path

--- src/pages/DevicesensorSettings.tsx
+++ src/pages/DevicesensorSettings.tsx
@@ -42,20 +42,48 @@ export const SelectElement = ({handleChange,title,conditions,isDisabled,matches,
 );
 function DeviceSensorSettings() {
     const [matches] = useOutletContext<[matches:boolean]>();
-    const {state} = useLocation();
-    const [sensor,setSensor] = useState<Sensor | null>(null)
+    const { pathname } = useLocation();
+    const {id,sensorId} = useParams();
+    const [device, setDevice] = useState<Device | null>(null);
+    const [sensOrActuator,setSensOrActuator] = useState<Sensor | Actuator | null>(null)
+    const [conditions,setConditions]= useState<string[]>([]);
+    // const [condy,setCondY] = useState<{id:string,name:string}[]>([]);
+    const [sensor,setSensor] = useState<Sensor |Actuator | null>(null)
     useEffect(() => {
-        window.wazigate.getSensor(state.sensorId).then(setSensor);
-    },[state]);
-    const handleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+        window.wazigate.getDevice(id).then(setDevice);
+        if(pathname.includes('actuators')){
+
+            window.wazigate.getActuator(sensorId as string).then((res)=>{
+                setSensOrActuator(res);
+                const rs= Object.keys(ontologies.actingDevices)
+                // const ids = Object.keys(rs);
+                // const names = Object.values(rs.Motor.label);
+                // const ress = ids.map((id,index)=>{
+                //     return {id:id,name:names[index]}
+                // })
+                // setCondY(ress);
+                setConditions(rs);
+                setSensor(res);
+            });
+        }else{
+            window.wazigate.getSensor(sensorId as string).then((res)=>{
+                setSensOrActuator(res);
+                const rs= Object.keys(ontologies.sensingDevices)
+                setConditions(rs);
+                setSensor(res);
+            });
+        }
+    },[id, pathname, sensorId]);
+    const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
         setSensor({
             ...sensor!,
             meta:{
                 ...sensor?.meta,
-                kind:event.target.value as string
+                [event.target.name]:event.target.value as string
             }
         })
     }
+    
     return (
         <Box height={'100%'}>
             <Box p={2} px={3}>
@@ -70,18 +98,18 @@ function DeviceSensorSettings() {
                                 <Link
                                     style={{fontSize:12}} 
                                     color="inherit"
-                                    to={"/device"+state.deviceId}
+                                    to={"/device"+device?.id}
                                 >
-                                    {state.deviceName}/
+                                    {device?.name}/
                                 </Link>
                             ):<Typography fontSize={15} color="text.primary">...</Typography>
                         }
                         <Link
                             style={{fontSize:12}}
                             color="inherit"
-                            to={"/device"+state.deviceId+"/sensors/"+state.sensorId}
+                            to={"/device"+device?.id+"/sensors/"+sensOrActuator?.id}
                         >
-                            {state.sensorname}
+                            {sensOrActuator?.name}
                         </Link>
                         <Typography fontSize={12} color="text.primary">Settings</Typography>
                     </Breadcrumbs>
@@ -90,24 +118,80 @@ function DeviceSensorSettings() {
             <Box bgcolor={matches?'#fff':'inherit'} height={'100%'} width={'100%'} px={2} pt={matches?2:2}  >
                 <Typography color={'#292F3F'}>Setup the sensor type, quantity and unit</Typography>
                 <Box my={2} width={matches?'30%':'100%'}>
-                    <SelectElement matches={matches} isDisabled={false} title={'Sensor'} handleChange={handleChange} conditions={[{id:'',name:''}]} value={sensor?.meta.kind} />
+                    <TextField id="standard-basic" value={(sensor)?.name} label="Name" variant="standard" />
+                    {
+                        
+                        pathname.includes('actuators')?(
+                            <>
+                                <SelectElement matches={matches} 
+                                    isDisabled={false} 
+                                    title={`${sensOrActuator?.name} Kind`} 
+                                    handleChange={handleChange} 
+                                    conditions={conditions} 
+                                    value={sensor?.meta.kind} 
+                                    name="kind"
+                                />
+                                <SelectElement matches={matches} 
+                                    isDisabled={false} 
+                                    title={`${sensOrActuator?.name} Quantity`} 
+                                    handleChange={handleChange} 
+                                    conditions={(ontologies.actingDevices)[sensor?.meta.kind as keyof typeof ontologies.actingDevices].quantities} 
+                                    value={sensor?.meta.quantity} 
+                                    name="quantity"
+                                />
+                                <SelectElement matches={matches} 
+                                    isDisabled={false} 
+                                    title={`${sensOrActuator?.name} Unit`} 
+                                    handleChange={handleChange} 
+                                    conditions={(ontologies.quantities)[sensor?.meta.quantity as keyof typeof ontologies.quantities].units} 
+                                    value={sensor?.meta.unit} 
+                                    name="unit"
+                                />
+                            </>
+                        ):(
+                            <>
+                                <SelectElement matches={matches} 
+                                    isDisabled={false} 
+                                    title={`${sensOrActuator?.name} Type`} 
+                                    handleChange={handleChange} 
+                                    conditions={(conditions)} 
+                                    value={sensor?.meta.kind} 
+                                    name="kind"
+                                />
+
+                                
+                            </>
+                        )
+                    }
                 </Box>
-                <Box width={matches?'30%':'90%'}>
-                    <Typography my={3} color={'#292F3F'}>Setup sync and sync interface</Typography>
-                    <RowContainerBetween additionStyles={{mt:3}}>
-                        <Typography fontSize={15} color={'#292F3F'}>Sync Sensor</Typography>
-                        <ToggleOff sx={{color:DEFAULT_COLORS.secondary_gray,fontSize:40, }} />
-                    </RowContainerBetween>
-                    <RowContainerBetween>
-                        <Typography fontSize={15} color={'#292F3F'}>Sync Interval</Typography>
-                        <ToggleOn sx={{color:DEFAULT_COLORS.primary_blue,fontSize:40, }} />
-                    </RowContainerBetween>
-                </Box>
-                <DiscreteSliderMarks matches={matches}/>
+                {
+                    pathname.includes('sensors')?(
+                        <>
+                            <Box width={matches?'30%':'90%'}>
+                                <Typography my={3} color={'#292F3F'}>Setup sync and sync interface</Typography>
+                                <RowContainerBetween additionStyles={{mt:3}}>
+                                    <Typography fontSize={15} color={'#292F3F'}>Sync Sensor</Typography>
+                                    <ToggleOff sx={{color:DEFAULT_COLORS.secondary_gray,fontSize:40, }} />
+                                </RowContainerBetween>
+                                <RowContainerBetween>
+                                    <Typography fontSize={15} color={'#292F3F'}>Sync Interval</Typography>
+                                    <ToggleOn sx={{color:DEFAULT_COLORS.primary_blue,fontSize:40, }} />
+                                </RowContainerBetween>
+                            </Box>
+                            <DiscreteSliderMarks matches={matches}/>
+                        </>
+                    ):(
+                        <>
+                            <Box width={matches?'30%':'90%'}>
+                                <TextField id="standard-basic" value={(sensor as Actuator).value?'Running':'Closed'} label="Add a value with the current time stamp" variant="standard" />
+                            </Box>
+                        </>
+                    )
+                }
                 <Box width={matches?'30%':'90%'}>
                     <RowContainerNormal>
                         <Button sx={{mx:1}} startIcon={<Save/>} variant={'contained'}>Save</Button>
-                        <Button sx={{mx:1,color:'#292F3F'}} variant={'text'}>Cancel</Button>
+                        <Button sx={{mx:1,color:'#292F3F'}} variant={'text'}>RESET</Button>
                     </RowContainerNormal>
                 </Box>
             </Box>
