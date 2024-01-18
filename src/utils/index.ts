f9d2c5c7f534ca50ad977a0bf1cf4342102ee859
Oliver Kipkemei <55626791+OliverMengich@users.noreply.github.com>
2024-01-18 19:15:32 +0300

    chore: helper functions for getting string length and capitalizing first letter

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,3 +4,12 @@ export function differenceInMinutes(date:Date){
     return Math.abs(Math.round(diff));
 
 }
+export function toStringHelper(value:string){
+    if(value){
+        return value.toString().length;
+    }
+    return 0;
+}
+export function capitalizeFirstLetter(value:string){
+    return value.charAt(0).toUpperCase() + value.slice(1);
+}
