55421dd3155a4d8948cba603082ebc1bf6200922
Oliver Kipkemei <55626791+OliverMengich@users.noreply.github.com>
2024-01-19 16:15:44 +0300

    chore: component for primary button

---
+++ src/components/shared/PrimaryButton.tsx
@@ -0,0 +1,13 @@
+import { Button,Typography, } from '@mui/material';
+interface Props{
+    title:string;
+    onClick?:()=>void;
+    type?:'submit'|'button'
+}
+export default function PrimaryButton({title,type, onClick}:Props) {
+    return (
+        <Button type={type} sx={{mx:1}} onClick={onClick} color="info" variant={'contained'}>
+            <Typography fontSize={14} fontWeight={'700'} color={'#fff'}>{title}</Typography>
+        </Button>
+    )
+}
