f6d7c01cbb4e2cb3319603b3a1dc9c2413ed59fe
Oliver Kipkemei <55626791+OliverMengich@users.noreply.github.com>
2024-01-23 09:55:11 +0300

    chore: adds option for setting button type

--- src/components/shared/PrimaryIconButton.tsx
+++ src/components/shared/PrimaryIconButton.tsx
@@ -2,11 +2,12 @@ import { Button,Typography,Icon } from '@mui/material';
 interface Props{
     title:string;
     iconname:string;
+    type?:'submit'|'button'
     onClick:()=>void;
 }
-export default function PrimaryIconButton({title,iconname,onClick}:Props) {
+export default function PrimaryIconButton({title,iconname, type, onClick}:Props) {
     return (
-        <Button startIcon={<Icon sx={{ color: '#fff' }} >{iconname}</Icon>} sx={{mx:1}} onClick={onClick} color="info" variant={'contained'}>
+        <Button type={type} startIcon={<Icon sx={{ color: '#fff' }} >{iconname}</Icon>} sx={{mx:1}} onClick={onClick} color="info" variant={'contained'}>
             <Typography fontSize={14} fontWeight={'700'} color={'#fff'}>{title}</Typography>
         </Button>
     )
