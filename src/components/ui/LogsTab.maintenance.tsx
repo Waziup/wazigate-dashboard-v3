35d7b6093d9b4c95a26934870b9a222a3d46defd
Oliver Kipkemei <55626791+OliverMengich@users.noreply.github.com>
2024-01-25 07:11:08 +0300

    chore: tab for showing logs

---
+++ src/components/ui/LogsTab.maintenance.tsx
@@ -0,0 +1,9 @@
+import { Box,Typography } from '@mui/material'
+
+export default function LogsTabMaintenance() {
+    return (
+        <Box>
+            <Typography>Logs tab</Typography>
+        </Box>
+    )
+}
