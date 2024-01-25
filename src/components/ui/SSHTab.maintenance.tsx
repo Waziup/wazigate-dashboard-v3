3c7a79288f7720645d908e384afacad8296758f2
Oliver Kipkemei <55626791+OliverMengich@users.noreply.github.com>
2024-01-25 07:10:05 +0300

    chore: tab for ssh

---
+++ src/components/ui/SSHTab.maintenance.tsx
@@ -0,0 +1,8 @@
+import { Box, Typography } from '@mui/material';
+export default function SSHTabMaintenance() {
+    return (
+        <Box>
+            <Typography>SSH tab</Typography>
+        </Box>
+    )
+}
