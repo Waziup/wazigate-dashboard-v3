61730d13f3b4788dc02211d5c4e87f0fffc85aa9
Oliver Kipkemei <55626791+OliverMengich@users.noreply.github.com>
2024-02-08 08:14:08 +0300

    feat: sets up the SSH iframe

--- src/components/ui/SSHTab.maintenance.tsx
+++ src/components/ui/SSHTab.maintenance.tsx
@@ -8,6 +8,7 @@ export default function SSHTabMaintenance({matches}:Props) {
     return (
         <Box>
             <Typography>SSH tab</Typography>
+            <iframe src="../ssh" style={{position: "absolute", width: "100%", height: "90%", border: "none" }} />
         </Box>
     )
 }
