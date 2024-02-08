6ac0362882109839b4309a499a19ce46ea452811
Oliver Kipkemei <55626791+OliverMengich@users.noreply.github.com>
2024-02-08 09:26:28 +0300

    feat: reusable menu Item

---
+++ src/components/shared/MenuDropDown.tsx
@@ -0,0 +1,41 @@
+import { MoreVert,  } from '@mui/icons-material';
+import { Box, Button, Menu,Icon, MenuItem, ListItemIcon } from '@mui/material';
+import PopupState, { bindTrigger, bindMenu } from 'material-ui-popup-state';
+import React from 'react'
+interface Props{
+    open:boolean
+    menuItems:{icon:string,text:string,clickHandler:()=>void}[]
+}
+export default function MenuComponent({open,menuItems}:Props) {
+    return (
+        <Box position={'relative'}>
+            <PopupState variant="popover" popupId="demo-popup-menu">
+                {(popupState) => (
+                    <React.Fragment>
+                        <Button id="demo-positioned-button"
+                            aria-controls={open ? 'demo-positioned-menu' : undefined}
+                            aria-haspopup="true"
+                            aria-expanded={open ? 'true' : undefined}
+                            {...bindTrigger(popupState)}
+                        >
+                            <MoreVert sx={{ color: 'black' }} />
+                        </Button>
+
+                        <Menu {...bindMenu(popupState)}>
+                            {
+                                menuItems.map((item,id)=>(
+                                    <MenuItem key={id} onClick={item.clickHandler}>
+                                        <ListItemIcon>
+                                            <Icon fontSize='small'>{item.icon}</Icon>
+                                        </ListItemIcon>
+                                        {item.text}
+                                    </MenuItem>
+                                ))
+                            }
+                        </Menu>
+                    </React.Fragment>
+                )}
+            </PopupState>
+        </Box>
+    )
+}
