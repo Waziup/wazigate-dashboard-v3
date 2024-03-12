3a32326e99321009ac99c0838fb6e7b86ae61630
Oliver Kipkemei <55626791+OliverMengich@users.noreply.github.com>
2024-03-12 13:23:26 +0300

    style: display style change

--- src/components/ui/ContainersTab.maintenance.tsx
+++ src/components/ui/ContainersTab.maintenance.tsx
@@ -1,8 +1,7 @@
 
-import { Backdrop, Box,  Grid, Typography } from '@mui/material';
+import { Backdrop, Box,  Button,  Grid, Typography } from '@mui/material';
 import RowContainerBetween from '../shared/RowContainerBetween';
 import React, { useEffect, useState } from 'react';
-import { Android12Switch } from '../shared/Switch';
 import RowContainerNormal from '../shared/RowContainerNormal';
 import { cInfo, getAllContainers, getContainerLogs } from '../../utils/systemapi';
 import MenuComponent from '../shared/MenuDropDown';
@@ -38,7 +37,7 @@ export default function ContainersTabMaintenance({matches}:Props) {
             console.log(res);
             setContainers(res);
         });
-    });
+    },[]);
     const handleClose = () => {
         setAnchorEl(null);
     };
@@ -46,9 +45,11 @@ export default function ContainersTabMaintenance({matches}:Props) {
         <>
             <Backdrop open={openModal}>
                 <Box width={matches ? '40%' : '90%'} bgcolor={'#fff'}>
-                    <Box borderBottom={'1px solid black'} px={2} py={2}>
+                    <RowContainerBetween additionStyles={{ borderBottom: '1px solid black', px: 2, py: 2 }}>
                         <Typography>Container Logs</Typography>
-                    </Box>
+                        <Button onClick={() =>{setOpenModal(false)}} sx={{ textTransform: 'initial', color: '#ff0000' }} variant={'text'} >cancel</Button>
+                        
+                    </RowContainerBetween>
                     {
                         logs.success? (
                             <Box maxWidth={'90%'} overflow={'auto'} width={'90%'} height={200} bgcolor={'#000'}>
@@ -70,13 +71,13 @@ export default function ContainersTabMaintenance({matches}:Props) {
                 <Grid container>
                     {
                         containers.map((container, id) => (
-                            <Grid key={id} lg={3.5} my={1} xl={4} md={4} xs={5.5} sm={3.5} item sx={{ bgcolor: '#fff', cursor: 'pointer', mx: matches ? 2 : 1, borderRadius: 2 }}>
+                            <Grid key={id} lg={4} my={1} xl={4} md={6} xs={12} sm={6} item sx={{ bgcolor: '#fff', cursor: 'pointer', borderRadius: 2 }}>
                                 <RowContainerBetween additionStyles={{ p: matches ? 1 : .3 }}>
                                     <RowContainerNormal>
                                         <Box component={'img'} src={'/docker.svg'} width={30} height={30} />
                                         <Box mx={.5}>
                                             <Typography fontSize={12}>{container.Names[0]}</Typography>
-                                            <Typography fontSize={10} color={'#666'}>Up 20 mins ago</Typography>
+                                            <Typography fontSize={10} color={'#666'}>{container.State}</Typography>
                                         </Box>
                                     </RowContainerNormal>
                                     <MenuComponent
@@ -92,13 +93,8 @@ export default function ContainersTabMaintenance({matches}:Props) {
                                 </RowContainerBetween>
                                 <Box p={2}>
                                     <Typography fontWeight={100} fontSize={15} color={'#2BBBAD'}>
-                                        Status:
-                                        <Typography component={'span'} fontSize={15} fontWeight={300} color={'#ff0000'}>{'   Stopped'}</Typography>
+                                        Status: {container.Status}
                                     </Typography>
-                                    <RowContainerBetween>
-                                        <Typography fontWeight={100} color={'rgba(0,0,0,.7)'} fontSize={15}>{'Start'}</Typography>
-                                        <Android12Switch checked={false} onChange={() => { }} color='info' />
-                                    </RowContainerBetween>
                                 </Box>
                             </Grid>
                         ))
