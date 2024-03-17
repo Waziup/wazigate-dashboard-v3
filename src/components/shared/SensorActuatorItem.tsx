8cd764d6e61f832c59c6b4366d234bef2e38ca4c
Oliver Kipkemei <55626791+OliverMengich@users.noreply.github.com>
2024-03-17 19:18:57 +0300

    fix:  removes special characters

--- src/components/shared/SensorActuatorItem.tsx
+++ src/components/shared/SensorActuatorItem.tsx
@@ -7,6 +7,7 @@ import ontologiesicons from '../../assets/ontologies.svg';
 import { Sensor,Actuator } from 'waziup';
 import ontologies from '../../assets/ontologies.json';
 import MenuComponent from './MenuDropDown';
+import { removeSpecialChars } from '../../utils';
 interface SensorActuatorItemProps {
     sensActuator: Sensor | Actuator;
     open:boolean,
@@ -17,20 +18,20 @@ interface SensorActuatorItemProps {
     deviceId:string
 }
 const isActuator = (sensActuator: Sensor | Actuator): sensActuator is Actuator => {
-    return Object.keys(ontologies.actingDevices).includes(sensActuator.meta.kind);
+    return Object.keys(ontologies.actingDevices).includes(sensActuator.meta.type);
 }
 export default function SensorActuatorItem({ sensActuator: sens,handleClose,open,children,deviceId}: SensorActuatorItemProps) {
     const [matches] = useOutletContext<[matches: boolean, matchesMd: boolean]>();
     const navigate = useNavigate();
     return (
-        <Grid lg={3} my={1} xl={3} md={4} xs={5.5} sm={5.5} onClick={()=>{navigate(`/devices/${deviceId}/${isActuator(sens)?'actuators':'sensors'}/${sens.id}`)}}  item sx={{ bgcolor: '#fff',cursor:'pointer', mx: matches?2:1, borderRadius: 2 }}>
+        <Grid lg={3} my={1} xl={3} md={4} xs={5.2} sm={5.2} onClick={()=>{navigate(`/devices/${deviceId}/${isActuator(sens)?'actuators':'sensors'}/${sens.id}`)}}  item sx={{ bgcolor: '#fff',cursor:'pointer', mx: matches?2:1, borderRadius: 2 }}>
             <RowContainerBetween additionStyles={{px:matches?1:.3}}>
                 <RowContainerNormal>
                     <SVGIcon
                         src={`${ontologiesicons}#${sens.meta.icon}`}
                         style={{ width: 20, height: 20, marginRight: 5 }}
                     />
-                    <Typography fontSize={12}>{sens.name}</Typography>
+                    <Typography fontSize={12}>{removeSpecialChars(sens? sens.name:'')}</Typography>
                 </RowContainerNormal>
                 <MenuComponent
                     open={open}
