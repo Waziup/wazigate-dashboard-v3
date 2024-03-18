11c83061eb75f56dcb446426a5270a0f2798e9b9
Oliver Kipkemei <55626791+OliverMengich@users.noreply.github.com>
2024-03-18 08:50:55 +0300

    fix: slider item

--- src/components/ui/DiscreteMarks.tsx
+++ src/components/ui/DiscreteMarks.tsx
@@ -13,19 +13,21 @@ const labels = ["0s", "5s", "10s", "20s", "30s", "1m", "2m", "5m", "10m", "15m",
 const marks = ["instant", "", "", "20s", "", "1m", "", "5m", "", "15m", "", "1h", "", "4h", "", "1D", "", "10D"];
 
 export default function DiscreteMarks({matches,onSliderChange,value}:{value:string, onSliderChange:(value:string)=>void, matches:boolean}) {
-    
+    const handleChange = (_event: Event, newValue: number | number[]) => {
+        onSliderChange(labels[newValue as number]);
+    }
     return (
         <Box sx={{ width: matches?'35%':'90%',mx:matches?2:'auto' }}>
             <Slider
-                aria-labelledby="sync-interval-input"
                 defaultValue={4}
-                getAriaValueText={valuetext}
                 valueLabelFormat={valueLabelFormat}
-                step={null}
-                max={marks.length-1}
-                value={labels.indexOf(value)}
-                onChange={(_e,value)=>onSliderChange(value.toString())}
                 sx={{color:DEFAULT_COLORS.primary_blue,fontSize:10, width:1,fontWeight:100}}
+                getAriaValueText={valuetext}
+                value={labels.indexOf(value)}
+                aria-labelledby="sync-interval-input"
+                step={null}
+                max={marks.length - 1}
+                onChange={handleChange}
                 valueLabelDisplay="auto"
                 marks={marks.map((label, value) => ({ value, label }))}
             />
