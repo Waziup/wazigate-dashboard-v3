142f5145295fbf1ba6aa6cb169a962649936d3c8
OliverMengich <kemeioliver2000@gmail.com>
2024-04-26 13:37:28 +0300

    fix: imports icons first

--- src/components/ui/CreateDeviceTab1.tsx
+++ src/components/ui/CreateDeviceTab1.tsx
@@ -34,8 +34,9 @@ export const DropDownCreateDeviceTab1 = ({handleChangeSelect,options,value}:{ ha
         
     </FormControl>
 );
+import WaziDevIcon from './wazidev.svg';
+import WaziActIcon from './WaziAct.svg';
 export default function CreateDeviceTab1({handleChange,newDevice,handleChangeSelect,}:{newDevice:Device,blockOnClick:(va:string)=>void,handleChange:(event: React.ChangeEvent<HTMLInputElement>)=>void,handleChangeSelect:(event: SelectChangeEvent<string>)=>void}){
-    
     return(
             <Box>
                 <FormControl sx={{my:1,width:'100%', borderBottom:'1px solid #292F3F'}}>
@@ -52,7 +53,7 @@ export default function CreateDeviceTab1({handleChange,newDevice,handleChangeSel
                 <DropDownCreateDeviceTab1 
                     value={newDevice.meta.type}
                     handleChangeSelect={handleChangeSelect}
-                    options={[{name:'Wazidev Board',id:'wazidev', imageurl:'./wazidev.svg'},{id:'genericboard',name:'Generic board',imageurl:'./WaziAct.svg'}]} 
+                    options={[{name:'Wazidev Board',id:'wazidev', imageurl:WaziDevIcon},{id:'genericboard',name:'Generic board',imageurl:WaziActIcon}]} 
                 />
                 
             </Box>
