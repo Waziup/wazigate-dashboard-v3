c23a027832e0dcf306705e166f70d51e17644ab6
OliverMengich <kemeioliver2000@gmail.com>
2024-04-29 11:14:46 +0300

    feat: apps icon

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,5 +1,5 @@
 import { Box, Grid, Stack, Typography } from "@mui/material";
-import { Router, CloudOff, Wifi, WaterDrop, } from '@mui/icons-material';
+import { Router, CloudOff, Wifi,  Cloud, } from '@mui/icons-material';
 import BasicTable from "../components/ui/BasicTable";
 import React, { useContext, useMemo, } from "react";
 import { DEFAULT_COLORS } from "../constants";
@@ -21,6 +21,9 @@ const DeviceStatus = ({ devices }: { devices: Device[] }) => (
     <Box sx={{ height: '100%', borderRadius: 2, bgcolor: 'white', p: 2 }}>
         <NormalText title="Device Status" />
         <BasicTable devices={devices} />
+        <Link style={{textDecoration:'none',textDecorationColor:'none',width:'100%', color:'#fff',borderBottom:'1px solid #fff',padding:'4px 0', borderTop:'1px solid white'}} to={'/devices'}>
+            <Typography sx={{ fontSize: 12,textAlign:'center', color: DEFAULT_COLORS.secondary_black, fontWeight: 300 }}>View all devices</Typography>
+        </Link>
     </Box>
 );
 const TextItem = ({ text }: { text: string }) => <Typography sx={{ fontSize: [10, 10, 12, 13, 10], color: DEFAULT_COLORS.secondary_black, fontWeight: 300 }} >{text}</Typography>
@@ -30,35 +33,55 @@ const AppStatus = ({ apps }: { apps: App[] }) => (
         <NormalText title="App Status" />
         <Stack width={'100%'} height={'100%'}>
             {
-                apps.map((app, index) => (
-                    <Link to={returnAppURL(app)} style={{textDecoration: 'none',cursor:'pointer' }} key={index}>
-                        <RowContainerBetween additionStyles={{":hover":{bgcolor:'#f5f5f5',cursor:'pointer',}}} key={index}>
-                            <RowContainerNormal >
-                                <WaterDrop sx={{ fontSize: [20, 35, 38, 40, 40], color: 'info.main' }} />
-                                <Box>
-                                    <Typography color={'black'} fontSize={[10, 12, 10, 12, 14]} fontWeight={300}>{app.name}</Typography>
-                                    <TextItem text="Last active 3h ago" />
-                                </Box>
-                            </RowContainerNormal>
-                            <Typography sx={{ 
-                                color: app.state ? app.state.running ? 'info.main' : '#CCC400' : 'info.main', 
-                                fontWeight: 300, 
-                                fontSize: [10, 12, 16, 12, 10] 
-                                }}>
+                apps.map((app, index) => {
+                    // eslint-disable-next-line react-hooks/rules-of-hooks
+                    const [imageError, setImageError] = React.useState(false);
+                    const handleImageError = () => {setImageError(true)}
+                    return(
+                        <Link to={returnAppURL(app)} style={{textDecoration: 'none',cursor:'pointer' }} key={index}>
+                            <RowContainerBetween additionStyles={{":hover":{bgcolor:'#f5f5f5',cursor:'pointer',}}} key={index}>
+                                <RowContainerNormal>
                                     {
-                                        app.state ? capitalizeFirstLetter(app.state.status) : 'Running'
+                                        imageError?(
+                                            <Box sx={{ width: 40, height: 40, borderRadius: 20, bgcolor: 'info.main', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                                                <Typography sx={{ fontSize: 15, color: 'white'}}>W</Typography>
+                                            </Box>
+                                        ):(app.waziapp && (app.waziapp as App['waziapp'] &{icon:string}).icon) ? (
+                                            <Box sx={{ width: 40, height: 40,alignItems:'center',display:'flex',justifyContent:'center', borderRadius: 20, overflow: 'hidden' }}>
+                                                <img onError={handleImageError} src={`/apps/${app.id}/`+(app.waziapp as App['waziapp'] &{icon:string}).icon} alt={app.name} style={{ width: 20, height: 20, borderRadius: 10 }} />
+                                            </Box>
+                                        ) : (
+                                            <Box sx={{ width: 40, height: 40, borderRadius: 20, bgcolor: 'info.main', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                                                <Typography sx={{ fontSize: 15, color: 'white'}}>W</Typography>
+                                            </Box>
+                                        )
                                     }
-                            </Typography>
-                        </RowContainerBetween>
-                    </Link>
-                ))
+                                    <Box>
+                                        <Typography color={'black'} fontSize={[10, 12, 10, 12, 14]} fontWeight={300}>{app.name}</Typography>
+                                        <TextItem 
+                                            text={'Created: '+(app.state !== null || app.state)?app.state?.startedAt?new Date(app.state.startedAt).toDateString():'':''}
+                                        />
+                                    </Box>
+                                </RowContainerNormal>
+                                <Typography sx={{ 
+                                    color: app.state ? app.state.running ? 'info.main' : '#CCC400' : 'info.main', 
+                                    fontWeight: 300, 
+                                    fontSize: [10, 12, 12, 12, 10] 
+                                    }}>
+                                        {
+                                            app.state ? capitalizeFirstLetter(app.state.status) : 'Running'
+                                        }
+                                </Typography>
+                            </RowContainerBetween>
+                        </Link>
+                    )})
             }
         </Stack>
     </Box>
 );
 export const NormalText = ({ title }: { title: string }) => (<Typography color={DEFAULT_COLORS.navbar_dark}>{title}</Typography>)
 function Dashboard() {
-    const { devices,networkDevices, apps } = useContext(DevicesContext);
+    const { devices,networkDevices,selectedCloud, apps } = useContext(DevicesContext);
     const [matches] = useOutletContext<[matches: boolean]>();
     const [apConn,eth0] = useMemo(() => {
         const apCn = networkDevices?.wlan0? networkDevices?.wlan0.AvailableConnections.find(conn => conn.connection.id === networkDevices.wlan0.ActiveConnectionId): null
@@ -76,8 +99,14 @@ function Dashboard() {
                                 <Item color={DEFAULT_COLORS.primary_blue} title="Gateway Status" more="Good" >
                                     <Router sx={{ mb: 2, fontSize: 42, color: 'black' }} />
                                 </Item>
-                                <Item color="#CCC400" title="Cloud Synchronization" more="Last active 3h ago" >
-                                    <CloudOff sx={{ mb: 2, fontSize: 42, color: '#D9D9D9' }} />
+                                <Item color={selectedCloud?.paused?"#CCC400":DEFAULT_COLORS.primary_blue} title="Cloud Synchronization" more={selectedCloud?.paused?"Inactive":'Active'} >
+                                    {
+                                        selectedCloud?.paused?(
+                                            <CloudOff sx={{ mb: 2, fontSize: 42, color: '#D9D9D9' }} />
+                                        ):(
+                                            <Cloud sx={{mb: 2, fontSize: 42, color: 'black' }} />
+                                        )
+                                    }
                                 </Item>
                                 {
                                     apConn?(
@@ -93,7 +122,7 @@ function Dashboard() {
                             </Stack>
                             <Grid mt={2} container spacing={2}>
                                 <Grid item py={6} sm={11} md={8} >
-                                    <DeviceStatus devices={devices} />
+                                    <DeviceStatus devices={devices.filter((_device, id) => id < 5)} />
                                 </Grid>
                                 <Grid py={6} item sm={12} md={4} >
                                     <AppStatus apps={apps} />
