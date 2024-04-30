76e12a433f693031e39aaa437a6006657dbf140e
OliverMengich <kemeioliver2000@gmail.com>
2024-04-30 13:42:26 +0300

    style: speed dial position

--- src/pages/Devices.tsx
+++ src/pages/Devices.tsx
@@ -304,136 +304,138 @@ function Devices() {
         }
     }
     return (
-        <Box sx={{ height: '100%', overflowY: 'auto' }}>
-            <CreateDeviceModalWindow
-                openModal={openModal}
-                handleToggleModal={handleToggleModal}
-                submitCreateDevice={submitCreateDevice}
-                handleChange={handleChange}
-                handleChangeSelect={handleChangeSelect}
-                selectedValue={selectedValue}
-                screen={screen}
-                handleScreenChange={handleScreenChange}
-                blockOnClick={blockOnClick}
-                newDevice={newDevice}
-                changeMakeLoraWAN={changeMakeLoraWAN}
-                handleChangeDeviceCodec={handleChangeDeviceCodec}
-                onTextInputChange={handleTextInputChange}
-                autoGenerateLoraWANOptionsHandler={autoGenerateLoraWANOptions}
-            />
-            <EditDeviceModal
-                device={selectedDevice as Device}
-                openModal={openEditModal}
-                handleChangeSelectDeviceType={handleChangeSelectDeviceType}
-                handleToggleModal={handleToggleEditModalClose}
-                handleChangeDeviceCodec={handleChangeDeviceCodec}
-                handleNameChange={handleEditSelectedDeviceName}
-                handleTextInputEditCodec={handleTextInputEditCodec}
-                submitEditDevice={handleSubmitEditDevice}
-                changeEditMakeLoraWAN={changeEditMakeLoraWAN}
-                autoGenerateLoraWANOptionsHandler={autoGenerateLoraWANOptions}
-            />
-            <Box sx={{ px:2, py:3,  height: '100%' }}>
-                <RowContainerBetween>
-                    <Typography fontWeight={700} color={'black'}>Devices</Typography>
-                    {
-                        matches?(
-                            <PrimaryIconButton title={'New Device'} iconname={'add'} onClick={handleToggleModal} />
-                        ):null
-                    }
-                </RowContainerBetween>
-                <Grid container my={2} spacing={2}>
-                    {
-                        devices.map((device, id) => {
-                            return (
-                                <Grid item m={1} key={id} md={5.5} lg={3.5} xl={3.5} sm={8} xs={12} >
-                                    <Box sx={{ boxShadow: 3, cursor: 'pointer', height: '100%', position: 'relative', bgcolor: 'white', borderRadius: 2, }}>
-                                        <Box sx={{ display: 'flex', alignItems: 'center', position: 'absolute', top: -8, my: -1, px: 1, py: .4, borderRadius: 1, mx: 1, bgcolor: DEFAULT_COLORS.primary_blue }}>
-                                            <Sensors sx={{ fontSize: 15, color: '#fff' }} />
-                                            <Typography fontSize={13} mx={1} color={'white'} component={'span'}>{device.meta ? capitalizeFirstLetter(device.meta.type) : ''}</Typography>
-                                        </Box>
-                                        <Box sx={{ borderBottom: '1px solid rgba(0,0,0,.1)', py: 1.5, ":hover": { py: 1.5 }, px: 1, }}>
-                                            <RowContainerBetween additionStyles={{}} >
-                                                <Box onClick={() => { navigate(`${device.id}`) }}>
-                                                    <Typography color={'info'} fontWeight={700}>{device.name.length > 10 ? device.name.slice(0, 10) + '....' : device.name}</Typography>
-                                                    <Typography color={DEFAULT_COLORS.secondary_black} fontSize={12} fontWeight={300}>Last Updated {(Math.round(differenceInMinutes(device.modified) / 60))>60?(Math.round(differenceInMinutes(device.modified) / 3600))+'hrs ago': Math.round(differenceInMinutes(device.modified) / 60)+'mins ago'}</Typography>
-                                                </Box>
-                                                <MenuComponent
-                                                    open={open}
-                                                    menuItems={[
-                                                        {
-                                                            clickHandler() {
-                                                                handleSelectDevice(device);
-                                                                handleClose();
+        <>
+            <Box sx={{ height: '100%', overflowY: 'auto' }}>
+                <CreateDeviceModalWindow
+                    openModal={openModal}
+                    handleToggleModal={handleToggleModal}
+                    submitCreateDevice={submitCreateDevice}
+                    handleChange={handleChange}
+                    handleChangeSelect={handleChangeSelect}
+                    selectedValue={selectedValue}
+                    screen={screen}
+                    handleScreenChange={handleScreenChange}
+                    blockOnClick={blockOnClick}
+                    newDevice={newDevice}
+                    changeMakeLoraWAN={changeMakeLoraWAN}
+                    handleChangeDeviceCodec={handleChangeDeviceCodec}
+                    onTextInputChange={handleTextInputChange}
+                    autoGenerateLoraWANOptionsHandler={autoGenerateLoraWANOptions}
+                />
+                <EditDeviceModal
+                    device={selectedDevice as Device}
+                    openModal={openEditModal}
+                    handleChangeSelectDeviceType={handleChangeSelectDeviceType}
+                    handleToggleModal={handleToggleEditModalClose}
+                    handleChangeDeviceCodec={handleChangeDeviceCodec}
+                    handleNameChange={handleEditSelectedDeviceName}
+                    handleTextInputEditCodec={handleTextInputEditCodec}
+                    submitEditDevice={handleSubmitEditDevice}
+                    changeEditMakeLoraWAN={changeEditMakeLoraWAN}
+                    autoGenerateLoraWANOptionsHandler={autoGenerateLoraWANOptions}
+                />
+                <Box sx={{ px:2, py:3,overflowY:'auto',width:'100%',  height: '100%' }}>
+                    <RowContainerBetween>
+                        <Typography fontWeight={700} color={'black'}>Devices</Typography>
+                        {
+                            matches?(
+                                <PrimaryIconButton title={'New Device'} iconname={'add'} onClick={handleToggleModal} />
+                            ):null
+                        }
+                    </RowContainerBetween>
+                    <Grid container my={2} spacing={2}>
+                        {
+                            devices.map((device, id) => {
+                                return (
+                                    <Grid item m={1} key={id} md={5.5} lg={3.5} xl={3.5} sm={8} xs={12} >
+                                        <Box sx={{ boxShadow: 3, cursor: 'pointer', height: '100%', position: 'relative', bgcolor: 'white', borderRadius: 2, }}>
+                                            <Box sx={{ display: 'flex', alignItems: 'center', position: 'absolute', top: -8, my: -1, px: 1, py: .4, borderRadius: 1, mx: 1, bgcolor: DEFAULT_COLORS.primary_blue }}>
+                                                <Sensors sx={{ fontSize: 15, color: '#fff' }} />
+                                                <Typography fontSize={13} mx={1} color={'white'} component={'span'}>{device.meta ? capitalizeFirstLetter(device.meta.type) : ''}</Typography>
+                                            </Box>
+                                            <Box sx={{ borderBottom: '1px solid rgba(0,0,0,.1)', py: 1.5, ":hover": { py: 1.5 }, px: 1, }}>
+                                                <RowContainerBetween additionStyles={{}} >
+                                                    <Box onClick={() => { navigate(`${device.id}`) }}>
+                                                        <Typography color={'info'} fontWeight={700}>{device.name.length > 10 ? device.name.slice(0, 10) + '....' : device.name}</Typography>
+                                                        <Typography color={DEFAULT_COLORS.secondary_black} fontSize={12} fontWeight={300}>Last Updated {(Math.round(differenceInMinutes(device.modified) / 60))>60?(Math.round(differenceInMinutes(device.modified) / 3600))+'hrs ago': Math.round(differenceInMinutes(device.modified) / 60)+'mins ago'}</Typography>
+                                                    </Box>
+                                                    <MenuComponent
+                                                        open={open}
+                                                        menuItems={[
+                                                            {
+                                                                clickHandler() {
+                                                                    handleSelectDevice(device);
+                                                                    handleClose();
+                                                                },
+                                                                icon:'mode_outlined',
+                                                                text:'Edit'
                                                             },
-                                                            icon:'mode_outlined',
-                                                            text:'Edit'
-                                                        },
-                                                        {
-                                                            clickHandler() {
-                                                                handleDeleteDevice(device);
-                                                                handleClose();
-                                                            },
-                                                            icon:'delete_outline',
-                                                            text:'Uninstall'
-                                                        }
-                                                    ]}
-                                                />
-                                            </RowContainerBetween>
-                                        </Box>
-                                        <CardContent sx={{ py: 0 }}>
-                                            {
-                                                device.sensors.length > 0 ? device.sensors.map((sensor) => (
-                                                    <Box key={sensor.id}>
-                                                        <SensorActuatorInfo
-                                                            onClick={() => {
-                                                                navigate(`/devices/${device.id}/sensors/${sensor.id}`, { state: { devicename: device.name, sensorId: sensor.id, deviceId: device.id, sensorname: sensor.name } })
+                                                            {
+                                                                clickHandler() {
+                                                                    handleDeleteDevice(device);
+                                                                    handleClose();
+                                                                },
+                                                                icon:'delete_outline',
+                                                                text:'Uninstall'
+                                                            }
+                                                        ]}
+                                                    />
+                                                </RowContainerBetween>
+                                            </Box>
+                                            <CardContent sx={{ py: 0 }}>
+                                                {
+                                                    device.sensors.length > 0 ? device.sensors.map((sensor) => (
+                                                        <Box key={sensor.id}>
+                                                            <SensorActuatorInfo
+                                                                onClick={() => {
+                                                                    navigate(`/devices/${device.id}/sensors/${sensor.id}`, { state: { devicename: device.name, sensorId: sensor.id, deviceId: device.id, sensorname: sensor.name } })
+                                                                }}
+                                                                iconname={sensor.meta.icon}
+                                                                name={sensor.name}
+                                                                text={sensor.value}
+                                                            />
+                                                        </Box>
+                                                    )) : (
+                                                        <Box my={2}></Box>
+                                                    )
+                                                }
+                                                {
+                                                    device.actuators.length > 0 ? device.actuators.map((act) => (
+                                                        <Box key={act.id}>
+                                                            <SensorActuatorInfo onClick={() => {
+                                                                navigate(`/devices/${device.id}/actuators/${act.id}`, { state: { deviceId: device.id, actuatordId: act.id, actuatorname: act.name } })
                                                             }}
-                                                            iconname={sensor.meta.icon}
-                                                            name={sensor.name}
-                                                            text={sensor.value}
-                                                        />
-                                                    </Box>
-                                                )) : (
-                                                    <Box my={2}></Box>
-                                                )
-                                            }
-                                            {
-                                                device.actuators.length > 0 ? device.actuators.map((act) => (
-                                                    <Box key={act.id}>
-                                                        <SensorActuatorInfo onClick={() => {
-                                                            navigate(`/devices/${device.id}/actuators/${act.id}`, { state: { deviceId: device.id, actuatordId: act.id, actuatorname: act.name } })
-                                                        }}
-                                                            iconname={act.meta.icon}
-                                                            name={act.name}
-                                                            text={act.meta.quantity==='Boolean' ? act.value ? 'Running' : 'Closed': act.value}
-                                                        />
-                                                    </Box>
-                                                )) : (
-                                                    <Box my={2}></Box>
-                                                )
-                                            }
-                                        </CardContent>
-                                    </Box>
-                                </Grid>
-                            )
-                        })
-                    }
-                </Grid>
-                {
-                    !matches ? (
-                        <SpeedDial color='info' ariaLabel='New Device' sx={{color:'#499', position: 'absolute', bottom: 16, right: 16 }} icon={<SpeedDialIcon />}>
-                            <SpeedDialAction
-                                key={'New Device'}
-                                icon={<Add />}
-                                onClick={handleToggleModal}
-                                tooltipTitle='New Device'
-                            />
-                        </SpeedDial>
-                    ): null
-                }
+                                                                iconname={act.meta.icon}
+                                                                name={act.name}
+                                                                text={act.meta.quantity==='Boolean' ? act.value ? 'Running' : 'Closed': act.value}
+                                                            />
+                                                        </Box>
+                                                    )) : (
+                                                        <Box my={2}></Box>
+                                                    )
+                                                }
+                                            </CardContent>
+                                        </Box>
+                                    </Grid>
+                                )
+                            })
+                        }
+                    </Grid>
+                </Box>
             </Box>
-        </Box>
+            {
+                !matches ? (
+                    <SpeedDial color={DEFAULT_COLORS.primary_blue} ariaLabel='New Device' sx={{color:'#499', position: 'absolute', bottom: 16, right: 16 }} icon={<SpeedDialIcon />}>
+                        <SpeedDialAction
+                            key={'New Device'}
+                            icon={<Add />}
+                            onClick={handleToggleModal}
+                            tooltipTitle='New Device'
+                        />
+                    </SpeedDial>
+                ): null
+            }
+        </>
     );
 }
 
