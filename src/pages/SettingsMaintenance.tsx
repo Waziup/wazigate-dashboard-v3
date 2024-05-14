0f0498074f14b005de890a684f4abbecccae394e
Oliver Kipkemei <55626791+OliverMengich@users.noreply.github.com>
2024-05-14 13:04:35 +0300

    feat: internet indicator

--- src/pages/SettingsMaintenance.tsx
+++ src/pages/SettingsMaintenance.tsx
@@ -6,6 +6,7 @@ import ContainersTabMaintenance from "../components/ui/ContainersTab.maintenance
 import ExportTabMaintenance from "../components/ui/ExportTab.maintenance";
 import LogsTabMaintenance from "../components/ui/LogsTab.maintenance";
 import { useOutletContext } from "react-router-dom";
+import InternetIndicator from "../components/ui/InternetIndicator";
 const BTN = ({title,icon,onClick,activeTab,children,idx}:{idx:string,title:string,activeTab:string,children?:React.ReactNode, icon?:string,onClick:(idx:string)=>void})=>(
     <Box  bgcolor={activeTab===idx?'#D1ECF1':'inherit'} >
         <Button onClick={()=>onClick(idx)} sx={{display:'flex',alignItems:'center', color:activeTab===idx?'#000':'#fff',py:1,px:2}}  variant="text" startIcon={
@@ -66,18 +67,21 @@ export default function SettingsMaintenance() {
     const TabComponent = tabs[activeTab].component;
     return (
         <Box sx={{ overflowY: 'auto', height: '100%',position:'relative' }}>
-            <Stack bgcolor={'primary.main'} overflow={'auto'}  direction={'row'} spacing={0}>
-                <BTN activeTab={activeTab} idx='0' onClick={handleTabChange} title={'Resources'} icon={'folder_copy'}/>
-                <BTN activeTab={activeTab} idx='1' onClick={handleTabChange} title={'SSH'} icon={'terminal_outlined'}/>
-                <BTN activeTab={activeTab} idx='2'onClick={handleTabChange} title={'Containers'}>
-                    <Box component={'img'} mr={.5} src="/docker.svg" color={activeTab?'#fff':'#000'} height={20} width={20} />
-                </BTN>
-                <BTN activeTab={activeTab} idx='3' onClick={handleTabChange} title={'Logs'} icon={'description'}/>
-                <Box minWidth={240}>
-                    <BTN activeTab={activeTab} idx='4' onClick={handleTabChange} title={'Export gateway data'}>
-                        <Box component={'img'} mr={.5} src="/export_notes.svg" color={activeTab?'#fff':'#000'} height={15} width={15} />
+            <Stack bgcolor={'primary.main'} overflow={'auto'} justifyContent={'space-between'}  direction={'row'} spacing={0}>
+                <Stack>
+                    <BTN activeTab={activeTab} idx='0' onClick={handleTabChange} title={'Resources'} icon={'folder_copy'}/>
+                    <BTN activeTab={activeTab} idx='1' onClick={handleTabChange} title={'SSH'} icon={'terminal_outlined'}/>
+                    <BTN activeTab={activeTab} idx='2'onClick={handleTabChange} title={'Containers'}>
+                        <Box component={'img'} mr={.5} src="/docker.svg" color={activeTab?'#fff':'#000'} height={20} width={20} />
                     </BTN>
-                </Box>
+                    <BTN activeTab={activeTab} idx='3' onClick={handleTabChange} title={'Logs'} icon={'description'}/>
+                    <Box minWidth={240}>
+                        <BTN activeTab={activeTab} idx='4' onClick={handleTabChange} title={'Export gateway data'}>
+                            <Box component={'img'} mr={.5} src="/export_notes.svg" color={activeTab?'#fff':'#000'} height={15} width={15} />
+                        </BTN>
+                    </Box>
+                </Stack>
+                <InternetIndicator/>
             </Stack>
             {
                 isPending?(
