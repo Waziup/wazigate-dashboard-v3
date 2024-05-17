3f1662d07231f8e41b5d9dfb78dba288571d7d4b
OliverMengich <kemeioliver2000@gmail.com>
2024-05-17 10:26:20 +0300

    feat: devEUI for device

--- src/components/ui/CreateDeviceTab2.tsx
+++ src/components/ui/CreateDeviceTab2.tsx
@@ -10,6 +10,8 @@ import { useContext } from "react";
 import { DevicesContext } from "../../context/devices.context";
 interface AddTextProps {
     text: string
+    isPlusHidden?: boolean
+    isReadOnly?: boolean
     placeholder: string
     textInputValue?: string
     onTextInputChange?: (e: React.ChangeEvent<HTMLInputElement>) => void
@@ -25,13 +27,17 @@ interface TabTwoProps {
     onTextInputChange?: (e: React.ChangeEvent<HTMLInputElement>) => void
     autoGenerateHandler: (title: "devAddr" | "nwkSEncKey" | "appSKey") => void
 }
-const AddTextShow = ({ text, name, autoGenerateHandler, placeholder, onTextInputChange, textInputValue }: AddTextProps) => (
+const AddTextShow = ({ text,isPlusHidden,isReadOnly, name, autoGenerateHandler, placeholder, onTextInputChange, textInputValue }: AddTextProps) => (
     <Box sx={{ my: 2 }}>
         <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', borderBottom: '1px solid #ccc' }}>
-            <input name={name} value={textInputValue} onInput={onTextInputChange} onChange={onTextInputChange} placeholder={text} style={{ border: 'none', color: '#757474', fontWeight: 200, outline: 'none', width: '100%', padding: '6px 0' }} />
-            <Tooltip onClick={() => autoGenerateHandler(name as "devAddr" | "nwkSEncKey" | "appSKey")} title="AutoGenerate">
-                <AddCircleOutline sx={{ color: '#292F3F', fontSize: 20 }} />
-            </Tooltip>
+            <input readOnly={isReadOnly} name={name} value={textInputValue} onInput={onTextInputChange} onChange={onTextInputChange} placeholder={text} style={{ border: 'none', color: '#757474', fontWeight: 200, outline: 'none', width: '100%', padding: '6px 0' }} />
+            {
+                isPlusHidden ? null : (
+                    <Tooltip onClick={() => autoGenerateHandler(name as "devAddr" | "nwkSEncKey" | "appSKey")} title="AutoGenerate">
+                        <AddCircleOutline sx={{ color: '#292F3F', fontSize: 20 }} />
+                    </Tooltip>
+                )
+            }
         </Box>
         <Typography fontSize={10} my={.5} color={'#292F3F'} fontWeight={200}>{placeholder}</Typography>
     </Box>
@@ -54,6 +60,7 @@ export default function CreateDeviceTabTwo({ onTextInputChange, newDevice, autoG
                         </RowContainerNormal>
                         {/* <SelectElementString mx={0} title={'Label'} handleChange={()=>{}} conditions={['Input','Level','Humidity']} value={'Temperature'} /> */}
                         <AddTextShow autoGenerateHandler={autoGenerateHandler} textInputValue={newDevice.meta.lorawan.devAddr} onTextInputChange={onTextInputChange} name="devAddr" text={'Device Addr (Device Address)'} placeholder={'8 digits required, got 0'} />
+                        <AddTextShow isPlusHidden autoGenerateHandler={autoGenerateHandler} textInputValue={newDevice.meta.lorawan.devEUI} onTextInputChange={onTextInputChange} name="devEUI" text={'Device EUI (Generated from Device address)'} placeholder={'Generated from Device address, got 0'} />
                         <AddTextShow autoGenerateHandler={autoGenerateHandler} textInputValue={newDevice.meta.lorawan.nwkSEncKey} onTextInputChange={onTextInputChange} name="nwkSEncKey" text={'NwkSKey(Network Session Key)'} placeholder={'32 digits required, got 0'} />
                         <AddTextShow autoGenerateHandler={autoGenerateHandler} textInputValue={newDevice.meta.lorawan.appSKey} onTextInputChange={onTextInputChange} name="appSKey" text={'AppKey (App Key)'} placeholder={'32 digits required, got 0'} />
                     </Box>
