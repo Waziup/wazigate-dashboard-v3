11318133610b8f35d9ea48abf0c2ab073f6a59a7
OliverMengich <kemeioliver2000@gmail.com>
2024-06-03 08:55:49 +0300

    fix: reroutes for authentication

--- src/App.tsx
+++ src/App.tsx
@@ -13,11 +13,12 @@ import Device from './pages/Device'
 import Docspage from './pages/Docspage'
 import DeviceSensorSettings from './pages/DevicesensorSettings';
 import { Box, createTheme, ThemeProvider } from '@mui/material'
-import DeviceSettings from './pages/DeviceSettings'
-import { DevicesProvider } from './context/devices.context'
+import DeviceSettings from './pages/DeviceSettings';
 import SettingsNetworking from './pages/SettingsNetworking'
 import SettingsMaintenance from './pages/SettingsMaintenance';
 import AppUI from './pages/App';
+import { useContext } from 'react'
+import { DevicesContext } from './context/devices.context'
 function App() {
     const theme = createTheme({
         palette: {
@@ -35,40 +36,46 @@ function App() {
             },
         },
     });
+    const { token } = useContext(DevicesContext);
     return (
-        <DevicesProvider>
             <ThemeProvider theme={theme}>
                 <Box bgcolor={'#F0F2F5'}>
                     <HashRouter>
                         <Routes>
-                            <Route >
-                                <Route path='/' element={<Login/>}/>
-                                <Route element={<Layout/>}>
-                                    <Route path='/dashboard' element={<Dashboard/>}/>
-                                    <Route path='/devices' element={<Devices/>}/>
-                                    <Route path='/devices/:id' element={<Device/>}/>
-                                    <Route path='/devices/:id/settings' element={<DeviceSettings/>}/>
-                                    <Route path='/devices/:id/sensors/:sensorId' element={<DeviceSensor/>}/>
-                                    <Route path='/devices/:id/sensors/:sensorId/settings' element={<DeviceSensorSettings/>}/>
-                                    <Route path='/devices/:id/actuators/:sensorId' element={<DeviceSensor/>}/>
-                                    <Route path='/devices/:id/actuators/:sensorId/settings' element={<DeviceSensorSettings/>}/>
-                                    <Route path='/apps' element={<Apps/>}/>
-                                    <Route path='/apps/:id/:id2/' element={<AppUI/>}/>
-                                    <Route path='/settings' element={<Settings/>}/>
-                                    <Route path='/settings/networking' element={<SettingsNetworking/>}/>
-                                    <Route path='/settings/maintenance' element={<SettingsMaintenance/>}/>
-                                    <Route path='/user' element={<User/>}/>
-                                    <Route path='/help' element={<Docspage/>}/>
-                                    <Route path='/docs' element={<Box height={'100vh'}><iframe width="100%" height="100%" className="app" src="/docs/" /></Box>}/>
-                                    <Route path='*' element={<div>Not Found</div>}/>
-                                </Route>
-                            </Route>
+                            <Route  path='*' element={<Login/>}
+                            />
+                            {
+                                token ? (
+                                    <Route >
+                                        <Route element={<Layout/>}>
+                                            <Route path='/dashboard' element={<Dashboard/>}/>
+                                            <Route path='/devices' element={<Devices/>}/>
+                                            <Route path='/devices/:id' element={<Device/>}/>
+                                            <Route path='/devices/:id/settings' element={<DeviceSettings/>}/>
+                                            <Route path='/devices/:id/sensors/:sensorId' element={<DeviceSensor/>}/>
+                                            <Route path='/devices/:id/sensors/:sensorId/settings' element={<DeviceSensorSettings/>}/>
+                                            <Route path='/devices/:id/actuators/:sensorId' element={<DeviceSensor/>}/>
+                                            <Route path='/devices/:id/actuators/:sensorId/settings' element={<DeviceSensorSettings/>}/>
+                                            <Route path='/apps' element={<Apps/>}/>
+                                            <Route path='/apps/:id/:id2/' element={<AppUI/>}/>
+                                            <Route path='/settings' element={<Settings/>}/>
+                                            <Route path='/settings/networking' element={<SettingsNetworking/>}/>
+                                            <Route path='/settings/maintenance' element={<SettingsMaintenance/>}/>
+                                            <Route path='/user' element={<User/>}/>
+                                            <Route path='/help' element={<Docspage/>}/>
+                                            <Route path='/docs' element={<Box height={'100vh'}><iframe width="100%" height="100%" className="app" src="/docs/" /></Box>}/>
+                                            <Route path='*' element={<div>Not Found</div>}/>
+                                        </Route>
+                                    </Route>
+                                ) : (
+                                    <Route path='/' element={<Login/>}/>
+                                )
+                            }
                         </Routes>
                         {/* <RouterProvider router={router}/> */}
                     </HashRouter>
                 </Box>
             </ThemeProvider>
-        </DevicesProvider>
     )
 }
 
