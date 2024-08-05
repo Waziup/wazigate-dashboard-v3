66173842ee053aaad3531658c8c0e4f17eabcf2c
OliverMengich <kemeioliver2000@gmail.com>
2024-08-05 17:01:27 +0300

    chore: displays actuator/sensor unit

--- src/components/shared/SensorActuatorInfo.tsx
+++ src/components/shared/SensorActuatorInfo.tsx
@@ -3,17 +3,20 @@ import RowContainerBetween from "./RowContainerBetween";
 import SVGIcon from "./SVGIcon";
 import ontologies from '../../assets/ontologies.json';
 import ontologiesicons from '../../assets/ontologies.svg';
-export default function SensorActuatorInfo({type, text, name,kind, onClick, iconname }: {kind:string, type:'sensor'|'actuator', text: string | number, name: string, onClick: () => void, iconname: string }) {
+export default function SensorActuatorInfo({type, text,unit, name,kind, onClick, iconname }: {unit:string, kind:string, type:'sensor'|'actuator', text: string | number, name: string, onClick: () => void, iconname: string }) {
     return (
         <RowContainerBetween onClick={onClick} additionStyles={{ my: 2, py: 1, px: .5, ":hover": { bgcolor: '#f5f5f5' } }}>
-            <Box sx={{ display: 'flex', width: '50%' }}>
+            <Box sx={{ display: 'flex',alignItems:'center', width: '70%' }}>
                 <SVGIcon 
-                    style={{ width: 18, height: 18, marginRight: 5 }}
+                    style={{ width: 35, height: 35, marginRight: 5 }}
                     src={`${ontologiesicons}#${iconname?iconname:type==='actuator'?ontologies.actingDevices[kind as  keyof typeof ontologies.actingDevices].icon:ontologies.sensingDevices[kind as  keyof typeof ontologies.sensingDevices].icon}`}
                 />
                 <Typography color={'primary'} ml={1} fontSize={12} fontWeight={300}>{name}</Typography>
             </Box>
-            <Typography color={'primary.main'} fontSize={14} fontWeight={300}>{text} </Typography>
+            <Box display={'flex'} alignItems={'center'}>
+                <Typography color={'primary.main'}mr={0.5} fontSize={14} fontWeight={300}>{text} </Typography>
+                <Typography color={'primary'} fontSize={12} fontWeight={300}>{unit}</Typography>
+            </Box>
         </RowContainerBetween>
     )
 }
