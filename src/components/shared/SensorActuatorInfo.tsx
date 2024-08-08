d4766fdeaa9ab37f7c0d55ee97fdb19201abe67d
OliverMengich <kemeioliver2000@gmail.com>
2024-08-08 14:02:28 +0300

    feat: feture for object values

--- src/components/shared/SensorActuatorInfo.tsx
+++ src/components/shared/SensorActuatorInfo.tsx
@@ -3,7 +3,19 @@ import RowContainerBetween from "./RowContainerBetween";
 import SVGIcon from "./SVGIcon";
 import ontologies from '../../assets/ontologies.json';
 import ontologiesicons from '../../assets/ontologies.svg';
+import { useMemo } from "react";
 export default function SensorActuatorInfo({type, text,unit, name,kind, onClick, iconname }: {unit:string, kind:string, type:'sensor'|'actuator', text: string | number, name: string, onClick: () => void, iconname: string }) {
+    const val = useMemo(() => {
+        if (typeof text === 'number') {
+            return (Math.round(text * 100) / 100).toFixed(2);
+        }else if(typeof text === 'object'){
+            const objectString = Object.entries(text)
+            .map(([key, value]) => `${key}:${value}`)
+            .join(', ');
+            return objectString
+        }
+        return text
+    },[text]);
     return (
         <RowContainerBetween onClick={onClick} additionStyles={{ my: 2, py: 1, px: .5, ":hover": { bgcolor: '#f5f5f5' } }}>
             <Box sx={{ display: 'flex',alignItems:'center', width: '70%' }}>
@@ -14,8 +26,8 @@ export default function SensorActuatorInfo({type, text,unit, name,kind, onClick,
                 <Typography color={'primary'} ml={1} fontSize={12} fontWeight={300}>{name}</Typography>
             </Box>
             <Box display={'flex'} alignItems={'center'}>
-                <Typography color={'primary.main'}mr={0.5} fontSize={14} fontWeight={300}>{text} </Typography>
-                <Typography color={'primary'} fontSize={12} fontWeight={300}>{unit}</Typography>
+                <Typography color={'primary.main'}mr={0.5} fontSize={14} fontWeight={300}>{val}</Typography>
+                <Typography color={'primary'} fontSize={12} fontWeight={300}>{typeof text==='object'?'':unit}</Typography>
             </Box>
         </RowContainerBetween>
     )
