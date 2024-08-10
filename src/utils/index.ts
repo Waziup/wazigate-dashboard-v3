c611afebc86c733ae3ebd580fe532ff7f129df4f
OliverMengich <kemeioliver2000@gmail.com>
2024-08-10 16:12:30 +0300

    fix: fixes time issue

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,8 +1,39 @@
 import { App, Device } from "waziup";
-export function differenceInMinutes(date:Date){
+const time_formats = [
+    [60, 'seconds', 1], // 60
+    [120, '1 minute ago', '1 minute from now'], // 60*2
+    [3600, 'minutes', 60], // 60*60, 60
+    [7200, '1 hour ago', '1 hour from now'], // 60*60*2
+    [86400, 'hours', 3600], // 60*60*24, 60*60
+    [172800, 'Yesterday', 'Tomorrow'], // 60*60*24*2
+    [604800, 'days', 86400], // 60*60*24*7, 60*60*24
+    [1209600, 'Last week', 'Next week'], // 60*60*24*7*4*2
+    [2419200, 'weeks', 604800], // 60*60*24*7*4, 60*60*24*7
+    [4838400, 'Last month', 'Next month'], // 60*60*24*7*4*2
+    [29030400, 'months', 2419200], // 60*60*24*7*4*12, 60*60*24*7*4
+    [58060800, 'Last year', 'Next year'], // 60*60*24*7*4*12*2
+    [2903040000, 'years', 29030400], // 60*60*24*7*4*12*100, 60*60*24*7*4*12
+    [5806080000, 'Last century', 'Next century'], // 60*60*24*7*4*12*100*2
+    [58060800000, 'centuries', 2903040000] // 60*60*24*7*4*12*100*20, 60*60*24*7*4*12*100
+];
+export function differenceInMinutes(date:Date | string){
     const now = new Date();
-    const diff = (now.getTime() - date.getTime()) / 1000;
-    return Math.abs(Math.round(diff));
+    const diffInSecconds = (now.getTime() - new Date(date).getTime()) / 1000;
+    if(diffInSecconds<60){
+        return Math.abs(Math.round(diffInSecconds))+' secs';
+    } else if(diffInSecconds<3600){
+        return Math.abs(Math.round(diffInSecconds/60))+' min';
+    } else if(diffInSecconds<86400){
+        return Math.abs(Math.round(diffInSecconds/3600))+' hrs';
+    } else if(diffInSecconds<604800){
+        return Math.abs(Math.round(diffInSecconds/86400))+' days';
+    } else if(diffInSecconds<2419200){
+        return Math.abs(Math.round(diffInSecconds/604800))+' weeks';
+    } else if(diffInSecconds<29030400){
+        return Math.abs(Math.round(diffInSecconds/2419200))+' months';
+    } else if(diffInSecconds<2903040000){
+        return Math.abs(Math.round(diffInSecconds/29030400))+' years';
+    }
 
 }
 export function toStringHelper(value:string){
@@ -33,6 +64,11 @@ export function allActiveDevices(devices: Device[]): number{
         }
     });
     return count;
+}
+export function orderByLastUpdated(devices: Device[]): Device[]{
+    return devices.sort((a,b)=>{
+        return a.modified.getTime() - b.modified.getTime();
+    });
 }
 export function humanFileSize(size: number ) {
     const  i = Math.floor(Math.log(size) / Math.log(1024));
@@ -81,23 +117,6 @@ export function time_ago(time: string | number | Date | null) {
         default:
             time = +new Date();
     }
-    const time_formats = [
-        [60, 'seconds', 1], // 60
-        [120, '1 minute ago', '1 minute from now'], // 60*2
-        [3600, 'minutes', 60], // 60*60, 60
-        [7200, '1 hour ago', '1 hour from now'], // 60*60*2
-        [86400, 'hours', 3600], // 60*60*24, 60*60
-        [172800, 'Yesterday', 'Tomorrow'], // 60*60*24*2
-        [604800, 'days', 86400], // 60*60*24*7, 60*60*24
-        [1209600, 'Last week', 'Next week'], // 60*60*24*7*4*2
-        [2419200, 'weeks', 604800], // 60*60*24*7*4, 60*60*24*7
-        [4838400, 'Last month', 'Next month'], // 60*60*24*7*4*2
-        [29030400, 'months', 2419200], // 60*60*24*7*4*12, 60*60*24*7*4
-        [58060800, 'Last year', 'Next year'], // 60*60*24*7*4*12*2
-        [2903040000, 'years', 29030400], // 60*60*24*7*4*12*100, 60*60*24*7*4*12
-        [5806080000, 'Last century', 'Next century'], // 60*60*24*7*4*12*100*2
-        [58060800000, 'centuries', 2903040000] // 60*60*24*7*4*12*100*20, 60*60*24*7*4*12*100
-    ];
     let seconds = (+new Date() - (time as number)) / 1000,
         token = 'ago',
         list_choice = 1;
@@ -113,12 +132,13 @@ export function time_ago(time: string | number | Date | null) {
     let i = 0,
       format;
     // eslint-disable-next-line no-cond-assign
-    while (format = time_formats[i++])
+    while (format = time_formats[i++]){
         if (seconds < Number(format[0])) {
             if (typeof format[2] == 'string')
                 return "Last updated "+format[list_choice];
             else
                 return "Last Updated "+Math.floor(seconds / format[2]) + ' ' + format[1] + ' ' + token;
         }
+    }
     return time;
   }
