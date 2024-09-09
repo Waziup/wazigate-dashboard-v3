8b4a202e3c995749f25458a71c971ae100e772ce
OliverMengich <kemeioliver2000@gmail.com>
2024-09-09 12:51:30 +0300

    fix: fixes path for ssh path and overflow style

--- src/components/ui/SSHTab.maintenance.tsx
+++ src/components/ui/SSHTab.maintenance.tsx
@@ -1,13 +1,8 @@
 import { Box, } from '@mui/material';
-interface Props{
-    matches?:boolean
-}
-export default function SSHTabMaintenance({matches}:Props) {
-    console.log(matches);
-    
+export default function SSHTabMaintenance() {
     return (
-        <Box>
-            <iframe src="/apps/waziup.wazigate-system/ssh" style={{position: "absolute", width: "100%", height: "90%", border: "none" }} />
+        <Box sx={{overflowY:'auto',height:'100%'}}>
+            <iframe src="/apps/waziup.wazigate-system/ssh/" style={{position: "absolute", width: "100%", height: "90%", border: "none" }} />
         </Box>
     )
 }
