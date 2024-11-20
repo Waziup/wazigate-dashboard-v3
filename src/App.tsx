ec8e938886514862be0f959a27bd319a5981cb51
OliverMengich <kemeioliver2000@gmail.com>
2024-11-20 10:10:03 +0300

    feat: style and path changed

--- src/App.tsx
+++ src/App.tsx
@@ -39,43 +39,43 @@ function App() {
     const { token } = useContext(DevicesContext);
     const creds = window.sessionStorage.getItem("creds");
     return (
-            <ThemeProvider theme={theme}>
-                <Box bgcolor={'#F0F2F5'}>
-                    <HashRouter>
-                        <Routes>
-                            {
-                                (token && creds) ? (
-                                    <Route >
-                                        <Route element={<Layout/>}>
-                                            <Route path='/' element={<Navigate to='/dashboard' replace />}/>
-                                            <Route path='/dashboard' element={<Dashboard/>}/>
-                                            <Route path='/devices' element={<Devices/>}/>
-                                            <Route path='/devices/:id' element={<Device/>}/>
-                                            <Route path='/devices/:id/settings' element={<DeviceSettings/>}/>
-                                            <Route path='/devices/:id/sensors/:sensorId' element={<DeviceSensor/>}/>
-                                            <Route path='/devices/:id/sensors/:sensorId/settings' element={<DeviceSensorSettings/>}/>
-                                            <Route path='/devices/:id/actuators/:sensorId' element={<DeviceSensor/>}/>
-                                            <Route path='/devices/:id/actuators/:sensorId/settings' element={<DeviceSensorSettings/>}/>
-                                            <Route path='/apps' element={<Apps/>}/>
-                                            <Route path='/apps/:id/:id2/' element={<AppUI/>}/>
-                                            <Route path='/settings' element={<Settings/>}/>
-                                            <Route path='/settings/networking' element={<SettingsNetworking/>}/>
-                                            <Route path='/settings/maintenance' element={<SettingsMaintenance/>}/>
-                                            <Route path='/user' element={<User/>}/>
-                                            <Route path='/help' element={<Docspage/>}/>
-                                            <Route path='/docs' element={<Box height={'100vh'}><iframe width="100%" height="100%" className="app" src="/docs/" /></Box>}/>
-                                            <Route path='*' element={<div>Not Found</div>}/>
-                                        </Route>
+        <ThemeProvider theme={theme}>
+            <Box bgcolor={'#F0F2F5'}>
+                <HashRouter>
+                    <Routes>
+                        {
+                            (token && creds) ? (
+                                <Route >
+                                    <Route element={<Layout/>}>
+                                        <Route path='/' element={<Navigate to='/dashboard' replace />}/>
+                                        <Route path='/dashboard' element={<Dashboard/>}/>
+                                        <Route path='/devices' element={<Devices/>}/>
+                                        <Route path='/devices/:id' element={<Device/>}/>
+                                        <Route path='/devices/:id/setting' element={<DeviceSettings/>}/>
+                                        <Route path='/devices/:id/sensors/:sensorId' element={<DeviceSensor/>}/>
+                                        <Route path='/devices/:id/sensors/:sensorId/setting' element={<DeviceSensorSettings/>}/>
+                                        <Route path='/devices/:id/actuators/:sensorId' element={<DeviceSensor/>}/>
+                                        <Route path='/devices/:id/actuators/:sensorId/setting' element={<DeviceSensorSettings/>}/>
+                                        <Route path='/apps' element={<Apps/>}/>
+                                        <Route path='/apps/:id/:id2/' element={<AppUI/>}/>
+                                        <Route path='/settings' element={<Settings/>}/>
+                                        <Route path='/settings/networking' element={<SettingsNetworking/>}/>
+                                        <Route path='/settings/maintenance' element={<SettingsMaintenance/>}/>
+                                        <Route path='/user' element={<User/>}/>
+                                        <Route path='/help' element={<Docspage/>}/>
+                                        <Route path='/docs' element={<Box sx={{overflow:'hidden',height:'100vh'}}><iframe width="100%" height="100%" className="app" src="/docs/" /></Box>}/>
+                                        <Route path='*' element={<div>Not Found</div>}/>
                                     </Route>
-                                ) : (
-                                    <Route path='/' element={<Login/>}/>
-                                )
-                            }
-                            <Route  path='*' element={<Login/>}  />
-                        </Routes>
-                    </HashRouter>
-                </Box>
-            </ThemeProvider>
+                                </Route>
+                            ) : (
+                                <Route path='/' element={<Login/>}/>
+                            )
+                        }
+                        <Route  path='*' element={<Login/>}  />
+                    </Routes>
+                </HashRouter>
+            </Box>
+        </ThemeProvider>
     )
 }
 
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -4,7 +4,7 @@ import { useLocation } from 'react-router-dom'
 export default function AppUI() {
     const {pathname}=useLocation()
     return (
-        <Box sx={{overflowY:'auto',height:'100%'}}>
+        <Box sx={{overflowY:'hidden',height:'100%'}}>
             <iframe src={pathname} title="Apps" width="100%" height="100%"></iframe>
         </Box>
     )
