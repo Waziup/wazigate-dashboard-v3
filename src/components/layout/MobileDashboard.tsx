df19973016dc5efb0111f92f1618b79c5bf547aa
OliverMengich <kemeioliver2000@gmail.com>
2024-11-21 09:35:06 +0300

    refactor: code changes for dashboard

--- src/components/layout/MobileDashboard.tsx
+++ src/components/layout/MobileDashboard.tsx
@@ -4,25 +4,14 @@ import { Box, Stack, Typography } from "@mui/material";
 import { DEFAULT_COLORS } from "../../constants";
 import RowContainerBetween from "../shared/RowContainerBetween";
 import RowContainerNormal from "../shared/RowContainerNormal";
-const Item = ({ more,path,onClick, color, children,showInternet,isSynched, title }: {isSynched?: boolean, path:string,showInternet?:boolean, onClick:(path: string)=>void, more: string, children: React.ReactNode, color: string, title: string }) => (
-    <Box onClick={()=>onClick(path)} sx={{minWidth:220, boxShadow: 3,mx:1,width:'30%', borderRadius: 1, border: '1px solid #ccc', height: '100%', bgcolor: 'white', p: 2 }}>
-        {children}
+const Item = ({ path, onClick, children,icon, title, }: { path:string, onClick:(path: string)=>void ,icon: React.ReactNode, children: React.ReactNode, title: string })=> (
+    <Box onClick={()=>onClick(path)} sx={{minWidth:220 ,mx:1,width:'30%', borderRadius: 1, border: '1px solid #ccc', height: '100%', bgcolor: 'white', p: 2 }}>
+        {icon}
         <Typography fontSize={13} color={'black'}>{title}</Typography>
-        <Typography color={color} fontSize={13} fontWeight={300}>{more}</Typography>
-        {
-            showInternet?(
-                <Box display={'flex'} >
-                    <Typography fontSize={14} fontWeight={300} color={DEFAULT_COLORS.secondary_black} mr={1}>Internet:   </Typography> <InternetIndicator />
-                </Box>
-            ):<Box >
-                <Typography fontSize={14} color={DEFAULT_COLORS.secondary_black} fontWeight={300}>
-                    {isSynched?'Synched with Waziup Cloud':'Not Synchronized'}
-                </Typography>
-            </Box>
-        }
+        {children}
     </Box>
 );
-import { capitalizeFirstLetter, isActiveDevice, returnAppURL, time_ago } from "../../utils";
+import { appChecker, capitalizeFirstLetter, isActiveDevice, returnAppURL, time_ago } from "../../utils";
 import { useNavigate, Link } from "react-router-dom";
 import { App, Cloud as Cl, Device as Dev } from "waziup";
 import type { Device, Connection } from "../../utils/systemapi";
@@ -45,49 +34,53 @@ export default function MobileDashboard({onClick,activeDevices,totalDevices, apC
     return (
         <Box sx={{ overflowY: 'auto', height: '100%' }} >
             <Stack direction={'row'} overflow={'auto'} m={2} spacing={1}>
-                <Item isSynched={selectedCloud?.paused?false: true} path='/settings/networking' onClick={onClick} color={selectedCloud?.paused?"#E9C68F":DEFAULT_COLORS.primary_blue} title="Cloud Synchronization" more={selectedCloud?.paused?"Inactive":'Active'} >
-                    {
-                        selectedCloud?.paused?(
-                            <CloudOff sx={{ fontSize: 20, color: '#D9D9D9' }} />
-                        ):(
-                            <Cloud sx={{ fontSize: 20, color: 'black' }} />
-                        )
-                    }
+                <Item icon={ selectedCloud?.paused?( <CloudOff sx={{ fontSize: 20, color: '#D9D9D9' }} />):(<Cloud sx={{ fontSize: 20, color: 'black' }} />) } path='/settings/networking' onClick={onClick} title="Cloud Synchronization" >
+                    <Typography fontSize={14} color={DEFAULT_COLORS.secondary_black} fontWeight={300}>
+                        {!(selectedCloud?.paused)?'Synched with Waziup Cloud':'Not Synchronized'}
+                    </Typography>
+                    <Typography color={selectedCloud?.paused?"#CCC400":DEFAULT_COLORS.primary_blue} fontSize={13} fontWeight={300}>{selectedCloud?.paused?"Inactive":'Active'}</Typography>
                 </Item>
                 {
                     (eth0 && eth0.IP4Config)?(
-                        <Item path='/settings/networking' showInternet onClick={onClick} color={DEFAULT_COLORS.secondary_black} title="Ethernet Connection" more={`IP Address: ${(eth0 && eth0.IP4Config)?eth0.IP4Config.Addresses[0].Address:''}`} >
-                            <Wifi sx={{ fontSize: 20, color: 'black' }} />
+                        <Item icon={<Wifi sx={{ fontSize: 20, color: 'black' }} />} path='/settings/networking' onClick={onClick} title="Ethernet Connection">
+                            <Typography color={DEFAULT_COLORS.secondary_black} fontSize={13} fontWeight={300}>
+                                {`IP Address: ${(eth0 && eth0.IP4Config)?eth0.IP4Config.Addresses[0].Address:''}`}
+                            </Typography>
+                            <RowContainerNormal additionStyles={{m:0}}>
+                                <Typography fontSize={14} fontWeight={300} color={DEFAULT_COLORS.secondary_black} mr={1}>Internet: </Typography>
+                                <InternetIndicator />
+                            </RowContainerNormal>
                         </Item>
                     ):(
-                        <Item path='/settings/networking' showInternet onClick={onClick} color={DEFAULT_COLORS.secondary_black} title="Wifi Connection" more={`Wifi Name: ${apConn?.connection.id}`} >
-                            <Wifi sx={{ fontSize: 20, color: 'black' }} />
+                        <Item icon={<Wifi sx={{ fontSize: 20, color: 'black' }} />} path='/settings/networking' onClick={onClick} title="Wifi Connection" >
+                            <Typography color={DEFAULT_COLORS.secondary_black} fontSize={13} fontWeight={300}>more={`Wifi Name: ${apConn?.connection.id}`}</Typography>
+                            <RowContainerNormal additionStyles={{m:0}}>
+                                <Typography fontSize={14} fontWeight={300} color={DEFAULT_COLORS.secondary_black} mr={1}>Internet: </Typography>
+                                <InternetIndicator />
+                            </RowContainerNormal>
                         </Item>
                     )
                 }
             </Stack>
-            <Box mt={2} px={1}>
-                <RowContainerBetween>
+            <Box mt={2} px={2}>
+                <RowContainerBetween >
                     <Typography color={'#666666'}>Device status</Typography>
                     <Link style={{textDecoration:'none', color:DEFAULT_COLORS.primary_blue,}} to={'/devices'}>
-                        <Typography fontSize={10} textAlign={'center'}>See all</Typography>
+                        <Typography fontSize={14} textAlign={'center'}>See all</Typography>
                     </Link>
                 </RowContainerBetween>
                 <RowContainerBetween>
                     <Box sx={{display:'flex',alignItems:'center',}}>
-                        <Typography fontSize={14} my={1} mr={1} color={DEFAULT_COLORS.secondary_black} fontWeight={300}>Total Devices: {totalDevices} </Typography>
-                        <Typography>  </Typography>
-                        <Typography fontSize={14} color={DEFAULT_COLORS.secondary_black} fontWeight={300}>Active Devices: {activeDevices}</Typography>
+                        <Typography fontSize={14} color={DEFAULT_COLORS.secondary_black} fontWeight={300}>{activeDevices} of {totalDevices} devices {activeDevices>1?'are':'is'} active.</Typography>
                     </Box>
                 <Box/>
                 </RowContainerBetween>
                 <Box display={'flex'} flexDirection={'column'} mt={1} py={1} alignItems={'center'}>
                     {
                         devices.length>0?devices.filter((_dev,idx)=>idx<5).map((dev, id) => (
-
                             <Box onClick={() => { handleNav(dev.id,dev.name) }} key={id} sx={{ cursor: 'pointer', my: 1, ":hover": { bgcolor: 'rgba(0,0,0,.1)' }, width: '100%', height: '100%', position: 'relative', bgcolor: 'white', borderRadius: 2, }}>
-                                <Box sx={{ position: 'absolute', top: -5, my: -1, borderRadius: 1, mx: 1, bgcolor: DEFAULT_COLORS.primary_blue }}>
-                                    <Typography fontSize={10} mx={1} color={'white'} component={'span'}>{capitalizeFirstLetter(dev.meta.type)}</Typography>
+                                <Box sx={{ position: 'absolute', top: -5, my: -1, borderRadius: 2, mx: 1, bgcolor: DEFAULT_COLORS.primary_blue }}>
+                                    <Typography fontSize={10} mx={1.2} color={'white'} component={'span'}>{(dev.meta && dev.meta.type)? capitalizeFirstLetter(dev.meta.type):'Generic'}</Typography>
                                 </Box>
                                 <Box sx={{ py: 1.5, px: 2, }}>
                                     <RowContainerBetween>
@@ -108,7 +101,7 @@ export default function MobileDashboard({onClick,activeDevices,totalDevices, apC
                     }
                 </Box>
             </Box>
-            <Box mt={1} px={1}>
+            <Box mt={1} px={2}>
                 <RowContainerBetween>
                     <Typography color={'#666666'}>App status</Typography>
                     <Link style={{textDecoration:'none', color:DEFAULT_COLORS.primary_blue,}} to={'/apps'}>
@@ -133,13 +126,10 @@ export default function MobileDashboard({onClick,activeDevices,totalDevices, apC
                                                 </Box>
                                             )
                                         }
-                                        <Box sx={{ width: 30, height: 30, borderRadius: 15, bgcolor: 'info.main', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-                                            <Typography sx={{ fontSize: 15, color: 'white'}}>W</Typography>
-                                        </Box>
                                         <Box mx={1}>
                                             <Typography fontSize={[12, 12, 16, 12, 10]} color={'black'} fontWeight={300}>{app.name}</Typography>
                                             <Typography fontSize={[10, 12, 10, 12, 14]} color={DEFAULT_COLORS.secondary_black} fontWeight={300}>
-                                            {'Created: '+(app.state !== null || app.state)?app.state?.startedAt?new Date(app.state.startedAt).toDateString():'':''}
+                                            {((app.state !== null || app.state)?appChecker(app.state):'')}
                                             </Typography>
                                         </Box>
                                     </RowContainerNormal>
