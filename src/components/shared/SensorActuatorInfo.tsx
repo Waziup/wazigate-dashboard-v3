1e4bae053c0db8e0365a2a7a9633d9ce7dca2c38
OliverMengich <kemeioliver2000@gmail.com>
2025-01-26 18:49:55 +0300

    feat: displays sensor/actuator last updated time

--- src/components/shared/SensorActuatorInfo.tsx
+++ src/components/shared/SensorActuatorInfo.tsx
@@ -1,10 +1,9 @@
 import { Box, Typography } from "@mui/material";
-import RowContainerBetween from "./RowContainerBetween";
 import SVGIcon from "./SVGIcon";
 import ontologies from '../../assets/ontologies.json';
 import ontologiesicons from '../../assets/ontologies.svg';
 import { useMemo } from "react";
-export default function SensorActuatorInfo({type, text,unit, name,kind, onClick, iconname }: {unit:string, kind:string, type:'sensor'|'actuator', text: string | number, name: string, onClick: () => void, iconname: string }) {
+export default function SensorActuatorInfo({type, text,unit, name,kind, onClick,lastUpdated, iconname }: {lastUpdated: string, unit:string, kind:string, type:'sensor'|'actuator', text: string | number, name: string, onClick: () => void, iconname: string }) {
     const val = useMemo(() => {
         if (typeof text === 'number') {
             return (Math.round(text * 100) / 100).toFixed(2);
@@ -26,18 +25,21 @@ export default function SensorActuatorInfo({type, text,unit, name,kind, onClick,
         }
     },[iconname,kind,type])
     return (
-        <RowContainerBetween onClick={onClick} additionStyles={{  px: 2,py:1.4,  ":hover": { bgcolor: '#f5f5f5' } }}>
+        <Box onClick={onClick} sx={{display:'flex',justifyContent:'space-between',  px: 2,py:1.4,  ":hover": { bgcolor: '#f5f5f5' } }}>
             <Box sx={{ display: 'flex',alignItems:'center', }}>
                 <SVGIcon 
                     style={{ width: 35, height: 35, }}
                     src={`${ontologiesicons}#${icon}`}
                 />
-                <Typography color={'primary'} ml={1} fontSize={12} fontWeight={300}>{name}</Typography>
+                <Box sx={{ml:1}}>
+                    <Typography color={'primary'}  fontSize={12} fontWeight={300}>{name}</Typography>
+                    <Typography color={'#797979'}  fontSize={12} fontWeight={400}>Last updated {lastUpdated}</Typography>
+                </Box>
             </Box>
             <Box display={'flex'} alignItems={'center'}>    
                 <Typography color={'primary.main'}mr={0.5} fontSize={14} fontWeight={300}>{val}</Typography>
                 <Typography color={'primary'} fontSize={12} fontWeight={300}>{typeof text==='object'?'':unit}</Typography>
             </Box>
-        </RowContainerBetween>
+        </Box>
     )
 }
