330b08c3feadea4689ed5ce55192f4542e7d51d4
OliverMengich <kemeioliver2000@gmail.com>
2025-03-26 12:52:31 +0300

    refactor: uses one dialog for edit and create device

--- src/components/ui/EditCreateDeviceDialog.tsx
+++ src/components/ui/EditCreateDeviceDialog.tsx
@@ -1,47 +1,48 @@
-import { RouterOutlined } from "@mui/icons-material";
-import { Box, Button, Dialog, DialogActions, DialogContent, DialogTitle, FormControl, Input, SelectChangeEvent, Stack, Typography } from "@mui/material";
-import { Device } from "waziup";
-import RowContainerBetween from "../shared/RowContainerBetween";
-import { DropDownCreateDeviceTab1 } from "./CreateDeviceTab1";
-import AddTextShow from "../shared/AddTextInput";
-import { Android12Switch } from "../shared/Switch";
-import React, { useContext } from "react";
-import { DevicesContext } from "../../context/devices.context";
-import { toStringHelper } from "../../utils";
-import { DEFAULT_COLORS } from "../../constants";
-import { InputField } from "../../pages/Login";
+import { RouterOutlined } from "@mui/icons-material"
+import { SelectChangeEvent, Dialog, Box, DialogTitle, DialogContent, FormControl, Input, Stack, Typography, DialogActions, Button } from "@mui/material"
+import { useContext } from "react"
+import { Device } from "waziup"
+import { DEFAULT_COLORS } from "../../constants"
+import { DevicesContext } from "../../context/devices.context"
+import { InputField } from "../../pages/Login"
+import { toStringHelper } from "../../utils"
+import AddTextShow from "../shared/AddTextInput"
+import RowContainerBetween from "../shared/RowContainerBetween"
+import { Android12Switch } from "../shared/Switch"
+import { DropDownCreateDeviceTab1 } from "./CreateDeviceTab1"
 
-interface EditDeviceModalProps {
+interface EditCreateDeviceDialogProps {
     handleNameChange: (e: React.ChangeEvent<HTMLInputElement>) => void,
     handleToggleModal: () => void,
     device: Device,
     openModal: boolean
     isFirst?: boolean
+    mode: "edit" | "create"
     matchesWidth?: boolean
     handleChangeSelectDeviceType: (e: SelectChangeEvent<string>) => void
-    handleTextInputEditCodec: (e: React.ChangeEvent<HTMLInputElement>) => void
+    handleTextInputChangeEditCreateDevice : (e: React.ChangeEvent<HTMLInputElement>) => void
     submitEditDevice: (e: React.FormEvent<HTMLFormElement>) => void
-    changeEditMakeLoraWAN: () => void,
+    changeEditMakeLoraWANDevice: () => void,
     autoGenerateLoraWANOptionsHandler: (title: "devAddr" | "nwkSEncKey" | "appSKey") => void
     handleChangeDeviceCodec: (event: SelectChangeEvent<string>) => void
 }
-export default function EditDeviceModal(props: EditDeviceModalProps) {
-    const { handleNameChange, handleToggleModal, device, openModal, isFirst, matchesWidth, handleChangeDeviceCodec, handleTextInputEditCodec, submitEditDevice, changeEditMakeLoraWAN, autoGenerateLoraWANOptionsHandler } = props
-    const { codecsList } = useContext(DevicesContext)
+export default function EditeCreateDeviceDialog(props: EditCreateDeviceDialogProps){
+    const { handleNameChange, handleToggleModal,mode, device, openModal, isFirst, matchesWidth, handleChangeDeviceCodec, handleTextInputChangeEditCreateDevice, submitEditDevice, changeEditMakeLoraWANDevice, autoGenerateLoraWANOptionsHandler } = props
+    const {codecsList} = useContext(DevicesContext);
     if (!device)
         return;
-    return (
-        <Dialog
-            fullWidth={matchesWidth ? undefined : true}
-            open={openModal}
-            onClose={handleToggleModal}
-            PaperProps={{
-                component: 'form',
-                onSubmit: (e: React.FormEvent<HTMLFormElement>) => { e.preventDefault(); submitEditDevice(e) }
-            }}
-        >
+    return(
+        <Dialog fullWidth={matchesWidth ? undefined : true}  open={openModal} onClose={handleToggleModal}  PaperProps={{ component: 'form', onSubmit: (e: React.FormEvent<HTMLFormElement>) => { e.preventDefault(); submitEditDevice(e) }  }} >
             <Box sx={{ bgcolor: 'background.paper', borderRadius: 2, width: matchesWidth ? 400 : undefined }}>
-                <DialogTitle>Edit Device: <em style={{ fontWeight: 400 }}>{device.name}</em></DialogTitle>
+                <DialogTitle>
+                    {
+                        mode==='edit'?(
+                            <p>Edit Device: <em style={{ fontWeight: 400 }}>{device.name}</em></p>
+                        ):(
+                            'Creating New Device'
+                        )
+                    }
+                </DialogTitle>
                 <DialogContent>
                     <FormControl sx={{ my: 0, width: '100%', }}>
                         <InputField label="Device Name" mendatory>
@@ -79,7 +80,7 @@ export default function EditDeviceModal(props: EditDeviceModalProps) {
                                     </Stack>
                                     <Android12Switch
                                         checked={device.meta.lorawan}
-                                        onChange={changeEditMakeLoraWAN}
+                                        onChange={changeEditMakeLoraWANDevice}
                                         color='secondary'
                                     />
                                 </RowContainerBetween>
@@ -89,7 +90,7 @@ export default function EditDeviceModal(props: EditDeviceModalProps) {
                                             <AddTextShow
                                                 autoGenerateHandler={autoGenerateLoraWANOptionsHandler}
                                                 name="devAddr"
-                                                onTextInputChange={handleTextInputEditCodec}
+                                                onTextInputChange={handleTextInputChangeEditCreateDevice}
                                                 textInputValue={device.meta.lorawan.devAddr}
                                                 text={'Device Addr (Device Address)'}
                                                 placeholder={'Device Address, 8 digits required, got ' + toStringHelper(device.meta.lorawan.devAddr)}
@@ -97,34 +98,28 @@ export default function EditDeviceModal(props: EditDeviceModalProps) {
                                             <AddTextShow
                                                 autoGenerateHandler={autoGenerateLoraWANOptionsHandler}
                                                 name="nwkSEncKey"
-                                                onTextInputChange={handleTextInputEditCodec}
+                                                onTextInputChange={handleTextInputChangeEditCreateDevice}
                                                 textInputValue={device.meta.lorawan.nwkSEncKey}
                                                 text={'NwkSKey(Network Session Key)'}
                                                 placeholder={'Network Session key 32 digits required, got ' + toStringHelper(device.meta.lorawan.nwkSEncKey)}
                                             />
                                             <AddTextShow
                                                 autoGenerateHandler={autoGenerateLoraWANOptionsHandler}
-                                                name="appSKey" onTextInputChange={handleTextInputEditCodec}
+                                                name="appSKey" onTextInputChange={handleTextInputChangeEditCreateDevice}
                                                 textInputValue={device.meta.lorawan.appSKey}
                                                 text={'AppKey (App Key)'}
                                                 placeholder={'App Key 32 digits required, got ' + toStringHelper(device.meta.lorawan.appSKey)}
                                             />
                                         </Box>
-                                    )}
+                                    )
+                                }
                             </>
                         )
                     }
                 </DialogContent>
                 <DialogActions>
-                    <Button
-                        onClick={handleToggleModal}
-                        variant={'text'}
-                        sx={{ mx: 1, color: '#ff0000' }}
-                        color={'info'}
-                    >
-                        Cancel
-                    </Button>
-                    <Button variant="text" type="submit" sx={{color:DEFAULT_COLORS.primary_blue}}>Save Changes</Button>
+                    <Button onClick={handleToggleModal} variant={'text'} sx={{ mx: 1, color: '#ff0000' }}  color={'info'}>Cancel</Button>
+                    <Button disabled={!device.name} variant="text" type="submit" sx={{color: (!device.name) ? '#d9d9d9' :DEFAULT_COLORS.primary_blue}}>Save Changes</Button>
                 </DialogActions>
             </Box>
         </Dialog>
