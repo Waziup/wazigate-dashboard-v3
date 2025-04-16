8b54ecc2281228590120ada7b29c99dbe5ccfc1b
OliverMengich <kemeioliver2000@gmail.com>
2025-04-16 09:16:33 +0300

    feat: adds copy to the text inputs

--- src/components/shared/AddTextInput.tsx
+++ src/components/shared/AddTextInput.tsx
@@ -1,7 +1,9 @@
-import { AutoFixNormal } from "@mui/icons-material";
-import { Box, Button, IconButton, Input, Tooltip, Typography } from "@mui/material";
+import { AutoFixNormal, ContentCopy } from "@mui/icons-material";
+import { Box, IconButton, Input, Stack, Tooltip, Typography } from "@mui/material";
+import { useState } from "react";
 
 interface AddTextProps {
+    color: string
     text: string;
     placeholder: string;
     textInputValue?: string;
@@ -17,6 +19,7 @@ export default function AddTextShow({
     text,
     isReadOnly,
     name,
+    color,
     matchesWidth,
     isPlusHidden,
     autoGenerateHandler,
@@ -24,61 +27,58 @@ export default function AddTextShow({
     onTextInputChange,
     textInputValue,
 }: AddTextProps) {
+    const [msg,setMsg] = useState("Copy to clipboard")
+    function copyToClipboard(text: string | undefined) {
+        navigator.clipboard.writeText(text??'')
+        .then(() =>{
+            setMsg("Copied to clipboard 👍👍");
+        })
+        .catch(err => {
+            setMsg("Could not copy "+err.toString())
+        });
+    }
     return (
-        <Box sx={{ my: 2, width: '100%' }}>
-            <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', gap: 1 }}>
-                <Input
-                    fullWidth={!matchesWidth}
-                    readOnly={isReadOnly}
-                    onChange={onTextInputChange}
-                    required
-                    name={name}
-                    value={textInputValue}
-                    placeholder={text}
-                    sx={{
-                        borderBottom: '1px solid #D5D6D8',
-                        '&:before, &:after': { borderBottom: 'none' },
-                        width: matchesWidth ? 'auto' : '100%',
-                        fontSize: '0.875rem',
-                    }}
-                />
-                {!isPlusHidden && (
-                    // <Button
-                    //     size="small"
-                    //     variant="contained"
-                    //     startIcon={<AutoFixNormal />}
-                    //     onClick={() => autoGenerateHandler && autoGenerateHandler(name as "devAddr" | "nwkSEncKey" | "appSKey")}
-                    //     disableElevation
-                    //     sx={{
-                    //         textTransform: 'none',
-                    //         backgroundColor: '#fdeee7',
-                    //         '&:hover': {
-                    //             backgroundColor: '#f25f16',
-                    //             color: '#fff',
-                    //         },
-                    //         padding: '4px 8px',
-                    //         fontSize: '0.875rem',
-                    //         minWidth: 'auto',
-                    //         whiteSpace: 'nowrap',
-                    //         color: '#f25f16',
-                    //     }}
-                    // >
-                    //     Generate
-                    // </Button>
+        <>
+            <Box sx={{ my: 2, width: '100%' }}>
+                <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', gap: 1 }}>
+                    <Input
+                        fullWidth={!matchesWidth}
+                        readOnly={isReadOnly}
+                        onChange={onTextInputChange}
+                        required
+                        name={name}
+                        value={textInputValue}
+                        placeholder={text}
+                        sx={{
+                            color: color,
+                            borderBottom: '1px solid #D5D6D8',
+                            '&:before, &:after': { borderBottom: 'none' },
+                            width: matchesWidth ? 'auto' : '100%',
+                            fontSize: '0.875rem',
+                        }}
+                    />
+                    <Stack direction="row">
+                        {!isPlusHidden ? (
+                            <Tooltip onClick={() => autoGenerateHandler && autoGenerateHandler(name as "devAddr" | "nwkSEncKey" | "appSKey")} title={"AutoGenerate " + name}>
 
-                    <Tooltip onClick={() => autoGenerateHandler && autoGenerateHandler(name as "devAddr" | "nwkSEncKey" | "appSKey")} title={"AutoGenerate " + name}>
-
-                        <IconButton
-                            aria-label="delete"
-                            size="small"
-                            onClick={() => autoGenerateHandler && autoGenerateHandler(name as "devAddr" | "nwkSEncKey" | "appSKey")}
-                        >
-                            <AutoFixNormal fontSize="inherit" />
-                        </IconButton>
-                    </Tooltip>
-                )}
+                                <IconButton
+                                    aria-label="delete"
+                                    size="small"
+                                    onClick={() => autoGenerateHandler && autoGenerateHandler(name as "devAddr" | "nwkSEncKey" | "appSKey")}
+                                >
+                                    <AutoFixNormal fontSize="inherit" />
+                                </IconButton>
+                            </Tooltip>
+                        ):null}
+                        <Tooltip onMouseLeave={() => setMsg("Copy to clipboard")} onClick={()=>copyToClipboard(textInputValue)} title={msg}>
+                            <IconButton aria-label="copy" size="small" onMouseLeave={() => setMsg("Copy to clipboard")} onClick={()=>copyToClipboard(textInputValue)}   >
+                                <ContentCopy fontSize="inherit"/>
+                            </IconButton>
+                        </Tooltip>
+                    </Stack>
+                </Box>
+                <Typography fontSize={10} my={0} color={'#292F3F'} fontWeight={200}>{placeholder}</Typography>
             </Box>
-            <Typography fontSize={10} my={0} color={'#292F3F'} fontWeight={200}>{placeholder}</Typography>
-        </Box>
+        </>
     );
 }
