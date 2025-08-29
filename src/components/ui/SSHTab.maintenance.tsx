5e20399a5414d54b87559054aed171e90353a202
Oliver Kipkemei <55626791+OliverMengich@users.noreply.github.com>
2025-08-29 10:46:52 +0300

    fix: SSH access on remote

--- src/components/ui/SSHTab.maintenance.tsx
+++ src/components/ui/SSHTab.maintenance.tsx
@@ -2,7 +2,7 @@ import { Box, } from '@mui/material';
 export default function SSHTabMaintenance() {
     return (
         <Box sx={{overflowY:'auto',height:'100%'}}>
-            <iframe src="/apps/waziup.wazigate-system/ssh/" style={{position: "absolute", width: "100%", height: "90%", border: "none" }} />
+            <iframe src="apps/waziup.wazigate-system/ssh/" style={{position: "absolute", width: "100%", height: "90%", border: "none" }} />
         </Box>
     )
 }
