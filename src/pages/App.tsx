a17e056f71a16b7e1a761aab24753b7f15a02e41
Oliver Kipkemei <55626791+OliverMengich@users.noreply.github.com>
2025-08-29 10:47:24 +0300

    fix: removes leading slash for relative path

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,11 +1,18 @@
 import { Box } from '@mui/material';
 import { useLocation } from 'react-router-dom'
 
+const removeLeadingLashes = (path: string)=>{
+    if (path.startsWith("/")) {
+        return path.replace(/^\/+/, "");
+    }
+    return path;
+}
+
 export default function AppUI() {
     const {pathname}=useLocation()
     return (
         <Box sx={{overflowY:'hidden',height:'100%'}}>
-            <iframe src={pathname} title="Apps" width="100%" height="100%"></iframe>
+            <iframe src={removeLeadingLashes(pathname)} title="Apps" width="100%" height="100%"></iframe>
         </Box>
     )
 }
