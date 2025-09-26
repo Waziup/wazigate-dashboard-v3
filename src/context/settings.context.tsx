2ff46014ff08a637cdc430b0094a8062b80d7825
Oliver Kipkemei <55626791+OliverMengich@users.noreply.github.com>
2025-09-26 08:44:21 +0300

    feat: fetching VPN info in context API

--- src/context/settings.context.tsx
+++ src/context/settings.context.tsx
@@ -1,31 +1,36 @@
 import React, { createContext, useContext, useEffect,useState } from "react";
 import { Cloud } from "waziup";
-import { Devices,getNetworkDevices } from "../utils/systemapi";
+import { Devices,getNetworkDevices, getVpnStatus, VPNStatus } from "../utils/systemapi";
 import { GlobalContext } from "./global.context";
 interface ContextValues{
     selectedCloud: Cloud | null
     setSelectedCloud:(cl: Cloud | null)=>void
     networkDevices: Devices
-    setNetWorkDevices:()=>void
+    vpnStatus: VPNStatus | null
+    setNetWorkDevices:()=>Promise<void>
 }
 
 export const SettingsContext = createContext<ContextValues>({
     selectedCloud:null,
+    vpnStatus: null,
     setSelectedCloud:()=>{},
     networkDevices: {},
-    setNetWorkDevices:()=>{},
+    setNetWorkDevices:async()=>{},
 });
 
 export const SettingsProvider = ({children}:{children:React.ReactNode})=>{
     const {token} = useContext(GlobalContext)
     const [networkDevices, setNetWorkDevices] = useState<Devices>({});
     const [selectedCloud, setSelectedCloud] = useState<Cloud | null>(null);
+    const [vpnStatus, setVPNStatus] = useState<VPNStatus | null>(null);
     const setNetWorkDevicesFc = async ()=>{
         await window.wazigate.getClouds().then((clouds) => {
             const waziupCloud = Object.values(clouds)? clouds['waziup']: null;
             setSelectedCloud(waziupCloud);
         });
         const netWorkDevs = await getNetworkDevices();
+        const vpnStatus = await getVpnStatus()
+        setVPNStatus(vpnStatus)
         setNetWorkDevices(netWorkDevs);
     };
     useEffect(()=>{
@@ -40,7 +45,8 @@ export const SettingsProvider = ({children}:{children:React.ReactNode})=>{
     
     const value: React.ProviderProps<ContextValues>['value'] = {
         networkDevices,
-        setNetWorkDevices:setNetWorkDevicesFc,
+        vpnStatus,
+        setNetWorkDevices: setNetWorkDevicesFc,
         setSelectedCloud,
         selectedCloud,
     }
